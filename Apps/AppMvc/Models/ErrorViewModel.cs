e26461407df7ff41967800cf99b20e0d57cabc20
Ben Green <ben@bcgdesign.com>
2019-12-13 14:46:18 +0000

    Marking model property as nullable to avoid warnings

--- Apps/AppMvc/Models/ErrorViewModel.cs
+++ Apps/AppMvc/Models/ErrorViewModel.cs
@@ -4,7 +4,7 @@ namespace MvcApp.Models
 {
 	public class ErrorViewModel
 	{
-		public string RequestId { get; set; }
+		public string? RequestId { get; set; }
 
 		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 	}
