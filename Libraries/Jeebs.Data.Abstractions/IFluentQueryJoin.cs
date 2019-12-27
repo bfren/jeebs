66facc01ccc5b1d17d844606c681627ed47696c5
Ben Green <ben@bcgdesign.com>
2019-12-27 12:02:00 +0000

    Revert "Removing FluentQuery - overly complex"
    This reverts commit 0c67586a9aafd6d7299e03896b9b02421d5fb08a.

---
+++ Libraries/Jeebs.Data.Abstractions/IFluentQueryJoin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jeebs.Data
+{
+	/// <summary>
+	/// Join for Query builder
+	/// </summary>
+	public interface IFluentQueryJoin<T>
+	{
+		/// <summary>
+		/// Set the ON column
+		/// </summary>
+		/// <param name="table">Table name</param>
+		/// <param name="column">Column name</param>
+		/// <returns>IQueryJoin object</returns>
+		IFluentQueryJoin<T> On(ITable table, string column);
+
+		/// <summary>
+		/// Set the EQUALS column
+		/// </summary>
+		/// <param name="column">Column name</param>
+		/// <returns>IQuery object</returns>
+		IFluentQuery<T> Equals(string column);
+	}
+}
---
+++ Libraries/Jeebs.Data.Abstractions/Query/FluentQueryParameters.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jeebs.Data
+{
+	/// <summary>
+	/// Fluent Query Parameters
+	/// </summary>
+	public sealed class FluentQueryParameters : Dictionary<string, object> { }
+}
