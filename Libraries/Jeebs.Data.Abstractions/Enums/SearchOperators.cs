56ef3bba7aa67242f76b8e3b540c86a6458ffc5f
Ben Green <ben@bcgdesign.com>
2020-03-13 18:26:16 +0000

    Adding query enums

---
+++ Libraries/Jeebs.Data.Abstractions/Enums/SearchOperators.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jeebs.Data.Enums
+{
+	/// <summary>
+	/// Search Operators
+	/// </summary>
+	public enum SearchOperators
+	{
+		/// <summary>
+		/// Equals
+		/// </summary>
+		Equals,
+
+		/// <summary>
+		/// Like
+		/// </summary>
+		Like
+	}
+}
---
+++ Libraries/Jeebs.Data.Abstractions/Enums/SortOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jeebs.Data.Enums
+{
+	/// <summary>
+	/// Sort order
+	/// </summary>
+	public enum SortOrder
+	{
+		/// <summary>
+		/// Ascending order
+		/// </summary>
+		Ascending,
+
+		/// <summary>
+		/// Descending order
+		/// </summary>
+		Descending
+	}
+}
