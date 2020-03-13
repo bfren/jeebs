90c2984e88436711567833e65025e461e4e26a2f
Ben Green <ben@bcgdesign.com>
2020-03-13 18:24:18 +0000

    Adding DateTimeExtensions - ToMySqlString()

---
+++ Libraries/Jeebs.Data/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jeebs.Data
+{
+	/// <summary>
+	/// DateTime Extensions
+	/// </summary>
+	public static class DateTimeExtensions
+	{
+		/// <summary>
+		/// Return a MySql formatted DateTime string for the specified date (yyyy-MM-dd HH:mm:ss)
+		/// </summary>
+		/// <param name="dt">DateTime object</param>
+		/// <returns>MySql Formatted string</returns>
+		public static string ToMySqlString(this DateTime dt) => dt.ToString("yyyy-MM-dd HH:mm:ss");
+	}
+}
