72ce81885cffe3736353e15c6ba28399bbd17567
Ben Green <ben@bcgdesign.com>
2020-08-27 18:56:54 +0100

    Adding GetPropertyInfo() tests

---
+++ Tests/Tests.Jeebs/Extensions/LinqExtensions/GetPropertyInfo_Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Xunit;
+
+namespace Jeebs.LinqExtensions_Tests
+{
+	public class GetPropertyInfo_Tests
+	{
+		[Fact]
+		public void Returns_PropertyInfo()
+		{
+			// Arrange
+			Expression<Func<Test, int>> expr = t => t.MyProperty;
+
+			// Act
+			var result = expr.GetPropertyInfo();
+
+			// Assert
+			Assert.IsType<PropertyInfo<Test, int>>(result);
+		}
+
+		public class Test
+		{
+			public int MyProperty { get; set; }
+		}
+	}
+}
