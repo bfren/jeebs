f0aef37d045b8fb9112508b76012db8bc846121e
Ben Green <ben@bcgdesign.com>
2020-09-04 13:36:42 +0100

    Removing JeebsF projects Updating Tests package

--- LibrariesF/JeebsF/All.fs
+++
@@ -1,5 +0,0 @@
-namespace JeebsF
-
-[<AutoOpen>]
-module Array =
-    let shuffle = Internal.Array.Shuffle.shuffle
--- LibrariesF/JeebsF/Core.fs
+++
@@ -1,7 +0,0 @@
-namespace JeebsF
-
-[<AutoOpen>]
-module Core =
-
-    let (|->) x f = printfn "%A" x ; f x
-    let (|=>) a b f = printfn "%A" a ; printfn "%A" b ; f a b
