6a63fff173b7d00d50272596cbbdb3e2f1fbde8e
Ben Green <ben@bcgdesign.com>
2020-10-08 16:08:46 +0100

    Adding random number generators and tests

---
+++ Libraries/Jeebs/Functions/MathsF.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace F
+{
+	/// <summary>
+	/// Maths shorthands
+	/// </summary>
+	public static class MathsF
+	{
+		/// <summary>
+		/// Returns a random number between 0 and 1
+		/// </summary>
+		public static double Random()
+		{
+			// Get a fresh provider
+			using var csp = new RNGCryptoServiceProvider();
+
+			// 8 bytes = 64-bit
+			byte[] b = new byte[8];
+			csp.GetBytes(b);
+			var dbl = (double)BitConverter.ToInt64(b, 0);
+
+			// Convert to a random number between 0 and 1
+			return dbl / long.MaxValue * (dbl < 0 ? -1 : 1);
+		}
+
+		/// <summary>
+		/// Returns a random integer between <paramref name="min"/> and <paramref name="max"/> inclusive
+		/// </summary>
+		/// <param name="min">Minimum acceptable value</param>
+		/// <param name="max">Maximum acceptable value</param>
+		public static long RandomInteger(long min = 0, long max = long.MaxValue)
+		{
+			// Check arguments
+			if (min >= max)
+			{
+				throw new ArgumentOutOfRangeException(nameof(min), min, "Minimium value must be less than the maximum value.");
+			}
+
+			if (min < 0)
+			{
+				throw new ArgumentException("Minimum value must be at least 0.", nameof(min));
+			}
+
+			// Get the range between the specified minimum and maximum values
+			var range = max - min;
+
+			// Now add a random amount of the range to the minimum value - it will never exceed maximum value
+			var add = Math.Round(range * Random());
+			return (long)(min + add);
+		}
+	}
+}
---
+++ Tests/Tests.Jeebs/Functions/MathsF/Random_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using static F.MathsF;
+
+namespace F.MathsF_Tests
+{
+	public class Random_Tests
+	{
+		[Fact]
+		public void Returns_Different_Number_Each_Time()
+		{
+			// Arrange
+			var numbers = new List<double>();
+
+			// Act
+			for (int i = 0; i < 1000000; i++)
+			{
+				numbers.Add(Random());
+			}
+
+			var unique = numbers.Distinct();
+
+			// Assert
+			Assert.Equal(unique.Count(), numbers.Count);
+			Assert.True(numbers.Min() >= 0);
+			Assert.True(numbers.Max() <= 1);
+		}
+	}
+}
