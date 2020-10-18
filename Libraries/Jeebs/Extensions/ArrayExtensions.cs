cc43fca281cdf40c16b80121997e8991b0c9f688
Ben Green <ben@bcgdesign.com>
2020-10-18 10:07:34 +0100

    Having issues with tests passing on VS!  Fixing Shuffle() extension method

--- Libraries/Jeebs/Extensions/ArrayExtensions.cs
+++ Libraries/Jeebs/Extensions/ArrayExtensions.cs
@@ -45,7 +45,7 @@ namespace Jeebs
 
 			for (int i = shuffled.Length; i > 1; i--)
 			{
-				int j = F.MathsF.RandomInt32(0, i, rng);
+				int j = F.MathsF.RandomInt32(max: i - 1, generator: rng);
 				T tmp = shuffled[j];
 				shuffled[j] = shuffled[i - 1];
 				shuffled[i - 1] = tmp;
