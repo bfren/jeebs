d136cb92199b0381e9784cd2e2d451d5655abbc3
Ben Green <ben@bcgdesign.com>
2020-10-18 09:15:45 +0100

    Using library random function for Array Shuffle

--- Libraries/Jeebs/Extensions/ArrayExtensions.cs
+++ Libraries/Jeebs/Extensions/ArrayExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Jeebs
@@ -36,12 +37,15 @@ namespace Jeebs
 		/// <returns>Shuffled array</returns>
 		public static T[] Shuffle<T>(this T[] @this)
 		{
+			// Don't alter the original array
 			var shuffled = @this.ToArray();
-			var random = new Random();
+
+			// For speed share the random number generator
+			using var rng = new RNGCryptoServiceProvider();
 
 			for (int i = shuffled.Length; i > 1; i--)
 			{
-				int j = random.Next(i);
+				int j = F.MathsF.RandomInt32(0, i, rng);
 				T tmp = shuffled[j];
 				shuffled[j] = shuffled[i - 1];
 				shuffled[i - 1] = tmp;
