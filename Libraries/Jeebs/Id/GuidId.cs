e660d3b276ef0a2441a8c3892cec8090d8c884c9
Ben Green <ben@bcgdesign.com>
2021-02-15 19:42:19 +0000

    Improving StrongId features

---
+++ Libraries/Jeebs/Id/GuidId.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jeebs
+{
+	/// <summary>
+	/// Guid ID
+	/// </summary>
+	/// <param name="Value">ID Value</param>
+	public abstract record GuidId(Guid Value) : StrongId<Guid>(Value)
+	{
+		/// <summary>
+		/// Default value
+		/// </summary>
+		public static Guid Default = Guid.Empty;
+
+		/// <inheritdoc cref="IStrongId.IsDefault"/>
+		public override bool IsDefault =>
+			Value == Default;
+
+		/// <summary>
+		/// Create with default value
+		/// </summary>
+		public GuidId() : this(Default) { }
+	}
+}
---
+++ Libraries/Jeebs/Id/StrongId.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jeebs
+{
+	/// <summary>
+	/// Strongly Typed ID record type
+	/// </summary>
+	/// <typeparam name="T">ID Value Type</typeparam>
+	/// <param name="Value">ID Value</param>
+	public abstract record StrongId<T>(T Value) : IStrongId<T>
+		where T : notnull
+	{
+		/// <inheritdoc cref="IStrongId.ValueStr"/>
+		public string ValueStr =>
+			Value.ToString() ?? typeof(T).ToString();
+
+		/// <inheritdoc cref="IStrongId.IsDefault"/>
+		public abstract bool IsDefault { get; }
+	}
+}
