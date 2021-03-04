889928281d55293fbb4db62f5e909fed37ba0f51
Ben Green <ben@bcgdesign.com>
2021-03-04 15:03:19 +0000

    Switching Option type to use records

--- Libraries/Jeebs.Option/Option.Some.cs
+++ Libraries/Jeebs.Option/Option.Some.cs
@@ -8,12 +8,12 @@ namespace Jeebs
 	/// 'Some' option - wraps value to enable safe non-null returns (see <seealso cref="None{T}"/>)
 	/// </summary>
 	/// <typeparam name="T"></typeparam>
-	public sealed class Some<T> : Option<T>
+	public sealed record Some<T> : Option<T>
 	{
 		/// <summary>
 		/// Option Value - nullability will match the nullability of <typeparamref name="T"/>
 		/// </summary>
-		public T Value { get; }
+		public T Value { get; private init; }
 
 		internal Some(T value) =>
 			Value = value;
