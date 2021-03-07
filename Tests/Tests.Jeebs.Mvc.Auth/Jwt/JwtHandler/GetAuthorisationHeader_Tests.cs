e0ce8300414499442c0268606912d5a2f0239709
Ben Green <ben@bcgdesign.com>
2021-03-07 10:16:18 +0000

    Updating file header template

--- Tests/Tests.Jeebs.Mvc.Auth/Jwt/JwtHandler/GetAuthorisationHeader_Tests.cs
+++ Tests/Tests.Jeebs.Mvc.Auth/Jwt/JwtHandler/GetAuthorisationHeader_Tests.cs
@@ -1,6 +1,44 @@
-﻿namespace Jeebs.Mvc.Auth.Jwt.JwtHandler_Tests
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Primitives;
+using NSubstitute;
+using Jm.Mvc.Auth.Jwt.JwtHandler;
+using Xunit;
+
+namespace Jeebs.Mvc.Auth.Jwt.JwtHandler_Tests
 {
 	public class GetAuthorisationHeader_Tests
 	{
+		[Fact]
+		public void Missing_Header_Returns_None_With_MissingAuthorisationHeaderMsg()
+		{
+			// Arrange
+			var headers = new Dictionary<string,StringValues>();
+
+			// Act
+			var result = JwtHandler.GetAuthorisationHeader(headers);
+
+			// Assert
+			var none = Assert.IsType<None<string>>(result);
+			Assert.IsType<MissingAuthorisationHeaderMsg>(none.Reason);
+		}
+		
+		[Fact]
+		public void Returns_Authorization_Header()
+		{
+            // Arrange
+			var value = F.Rnd.Str;
+            var headers = new Dictionary<string, StringValues>
+            {
+                { "Authorization", value }
+            };
+
+            // Act
+            var result = JwtHandler.GetAuthorisationHeader(headers);
+
+			// Assert
+			var some = Assert.IsType<Some<string>>(result);
+			Assert.Equal(value, some.Value);
+		}
 	}
 }
