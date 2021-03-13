7d4b37deb56da5d3cb8366f0f52342a745059065
Ben Green <ben@bcgdesign.com>
2021-03-13 14:24:23 +0000

    Adding RoleEntity properties

--- Libraries/Jeebs.Auth.Data.Entities/RoleEntity.cs
+++ Libraries/Jeebs.Auth.Data.Entities/RoleEntity.cs
@@ -3,7 +3,26 @@
 
 namespace Jeebs.Auth.Data.Entities
 {
-	public class RoleEntity
+	/// <inheritdoc cref="IRole"/>
+	internal sealed record RoleEntity : IRole
 	{
+		/// <inheritdoc/>
+		public IStrongId<long> Id
+		{
+			get =>
+				RoleId;
+
+			init =>
+				RoleId = new RoleId(value.Value);
+		}
+
+		/// <inheritdoc/>
+		public RoleId RoleId { get; init; } = new RoleId();
+
+		/// <inheritdoc/>
+		public string Name { get; init; } = string.Empty;
+
+		/// <inheritdoc/>
+		public string Description { get; init; } = string.Empty;
 	}
 }
