209c061b4770362c58305fa26c7382cd69e63c3c
Ben Green <ben@bcgdesign.com>
2021-03-22 16:57:26 +0000

    Running code cleanup

--- src/Libraries/Jeebs.Random/Rnd.ByteF.cs
+++ src/Libraries/Jeebs.Random/Rnd.ByteF.cs
@@ -1,7 +1,6 @@
 ﻿// Jeebs Rapid Application Development
 // Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
 
-using System;
 using System.Security.Cryptography;
 
 namespace F
--- src/Tests/Tests.Jeebs.Data/TypeHandlers/StrongId/Parse_Tests.cs
+++ src/Tests/Tests.Jeebs.Data/TypeHandlers/StrongId/Parse_Tests.cs
@@ -1,7 +1,6 @@
 ﻿// Jeebs Unit Tests
 // Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
 
-using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace Jeebs.Data.TypeHandlers.StrongId_Tests
