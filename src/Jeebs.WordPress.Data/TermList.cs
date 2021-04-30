ce6095dd6d33e0155875308b8f14bb368e01ccd2
Ben Green <ben@bcgdesign.com>
2021-04-30 15:28:51 +0100

    Adding TermList class

---
+++ src/Jeebs.WordPress.Data/TermList.cs
@@ -0,0 +1,64 @@
+﻿// Jeebs Rapid Application Development
+// Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
+
+using System.Collections.Generic;
+using Jeebs.WordPress.Data.Entities;
+using Jeebs.WordPress.Data.Enums;
+
+namespace Jeebs.WordPress.Data
+{
+	/// <summary>
+	/// Term List - allows taxonomies to be added to posts
+	/// </summary>
+	public sealed class TermList : List<TermList.Term>
+	{
+		/// <summary>
+		/// Taxonomy of this Term List
+		/// </summary>
+		public Taxonomy Taxonomy { get; }
+
+		/// <summary>
+		/// Create object
+		/// </summary>
+		/// <param name="taxonomy">Taxonomy of these terms</param>
+		public TermList(Taxonomy taxonomy) =>
+			Taxonomy = taxonomy;
+
+		/// <summary>
+		/// Term Model
+		/// </summary>
+		public abstract record Term : IWithId<WpTermId>
+		{
+			/// <summary>
+			/// TermId
+			/// </summary>
+			public WpTermId Id { get; init; } = new();
+
+			/// <summary>
+			/// TermId
+			/// </summary>
+			public long TermId { get; init; }
+
+			/// <summary>
+			/// Title
+			/// </summary>
+			public string Title { get; init; } = string.Empty;
+
+			/// <summary>
+			/// Slug
+			/// </summary>
+			public string Slug { get; init; } = string.Empty;
+
+			/// <summary>
+			/// Count
+			/// </summary>
+			public long Count { get; init; }
+
+			/// <summary>
+			/// Display title
+			/// </summary>
+			public override string ToString() =>
+				Title;
+		}
+	}
+}
