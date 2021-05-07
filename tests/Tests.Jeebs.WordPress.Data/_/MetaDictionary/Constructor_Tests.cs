3e86e78e4859d3f606858afdae209f90f4ff30aa
Ben Green <ben@bcgdesign.com>
2021-05-07 12:05:14 +0100

    Adding MetaDictionary tests

---
+++ tests/Tests.Jeebs.WordPress.Data/_/MetaDictionary/Constructor_Tests.cs
@@ -0,0 +1,44 @@
+﻿// Jeebs Unit Tests
+// Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
+
+using System.Collections.Generic;
+using Xunit;
+
+namespace Jeebs.WordPress.Data.MetaDictionary_Tests
+{
+	public class Constructor_Tests
+	{
+		[Fact]
+		public void Concatenates_Duplicate_Items()
+		{
+			// Arrange
+			var k0 = F.Rnd.Str;
+			var k1 = F.Rnd.Str;
+			var v0 = F.Rnd.Str;
+			var v1 = F.Rnd.Str;
+			var v2 = F.Rnd.Str;
+
+			// Act
+			var result = new MetaDictionary(new KeyValuePair<string, string>[]
+			{
+				new(k0, v0),
+				new(k0, v1),
+				new(k1, v2)
+			});
+
+			// Assert
+			Assert.Collection(result,
+				x =>
+				{
+					Assert.Equal(k0, x.Key);
+					Assert.Equal($"{v0};{v1}", x.Value);
+				},
+				x =>
+				{
+					Assert.Equal(k1, x.Key);
+					Assert.Equal(v2, x.Value);
+				}
+			);
+		}
+	}
+}
