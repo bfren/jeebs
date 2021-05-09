1e0b6905a83e001e4bf18dcd6cf753160640b419
Ben Green <ben@bcgdesign.com>
2021-05-09 13:37:32 +0100

    Adding GetParts tests

---
+++ tests/Tests.Jeebs.Data/- Test Abstracts -/QueryOptions/GetParts.cs
@@ -0,0 +1,118 @@
+﻿// Jeebs Unit Tests
+// Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
+
+using System.Collections.Generic;
+using Jeebs.Data.Enums;
+using Jeebs.Data.Mapping;
+using NSubstitute;
+using Xunit;
+
+namespace Jeebs.Data.Querying.QueryOptions_Tests
+{
+	public abstract class GetParts<TOptions, TId> : QueryOptions_Tests<TOptions, TId>
+		where TOptions : QueryOptions<TId>
+		where TId : StrongId, new()
+	{
+		public abstract void Test00_Adds_Id();
+
+		protected void Test00()
+		{
+			// Arrange
+			var id = new TId { Value = F.Rnd.Lng };
+			var (options, v) = Setup(opt => opt with { Id = id });
+			var cols = Substitute.For<IColumnList>();
+
+			// Act
+			var result = options.GetPartsTest(v.Table, cols, v.IdColumn);
+
+			// Assert
+			var some = result.AssertSome();
+			Assert.Collection(some.Where,
+				x =>
+				{
+					Assert.Same(v.IdColumn, x.column);
+					Assert.Equal(Compare.Equal, x.cmp);
+					Assert.Equal(id.Value, x.value);
+				}
+			);
+		}
+
+		public abstract void Test01_Adds_Ids();
+
+		protected void Test01()
+		{
+			// Arrange
+			var i0 = new TId { Value = F.Rnd.Lng };
+			var i1 = new TId { Value = F.Rnd.Lng };
+			var (options, v) = Setup(opt => opt with { Ids = new[] { i0, i1 } });
+			var cols = Substitute.For<IColumnList>();
+
+			// Act
+			var result = options.GetPartsTest(v.Table, cols, v.IdColumn);
+
+			// Assert
+			var some = result.AssertSome();
+			Assert.Collection(some.Where,
+				x =>
+				{
+					Assert.Same(v.IdColumn, x.column);
+					Assert.Equal(Compare.In, x.cmp);
+
+					var value = Assert.IsAssignableFrom<IEnumerable<long>>(x.value);
+					Assert.Collection(value,
+						y => Assert.Equal(i0.Value, y),
+						y => Assert.Equal(i1.Value, y)
+					);
+				}
+			);
+		}
+
+		public abstract void Test02_Adds_SortRandom();
+
+		protected void Test02()
+		{
+			// Arrange
+			var (options, v) = Setup(opt => opt with { SortRandom = true });
+			var cols = Substitute.For<IColumnList>();
+
+			// Act
+			var result = options.GetPartsTest(v.Table, cols, v.IdColumn);
+
+			// Assert
+			var some = result.AssertSome();
+			Assert.True(some.SortRandom);
+		}
+
+		public abstract void Test03_Adds_Sort();
+
+		protected void Test03()
+		{
+			// Arrange
+			var c0 = Substitute.For<IColumn>();
+			var o0 = SortOrder.Ascending;
+			var c1 = Substitute.For<IColumn>();
+			var o1 = SortOrder.Descending;
+			var sort = new[] { (c0, o0), (c1, o1) };
+			var (options, v) = Setup(opt => opt with { Sort = sort });
+			var cols = Substitute.For<IColumnList>();
+
+			// Act
+			var result = options.GetPartsTest(v.Table, cols, v.IdColumn);
+
+			// Assert
+			var some = result.AssertSome();
+			Assert.Collection(some.Sort,
+				x =>
+				{
+					Assert.Same(c0, x.column);
+					Assert.Equal(o0, x.order);
+				},
+				x =>
+				{
+					Assert.Same(c1, x.column);
+					Assert.Equal(o1, x.order);
+				}
+			);
+		}
+	}
+}
--- tests/Tests.Jeebs.Data/Querying/QueryOptions/GetParts_Tests.cs
+++ tests/Tests.Jeebs.Data/Querying/QueryOptions/GetParts_Tests.cs
@@ -1,113 +1,30 @@
 ﻿// Jeebs Unit Tests
 // Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
 
-using System.Collections.Generic;
-using Jeebs.Data.Enums;
 using Jeebs.Data.Mapping;
-using NSubstitute;
 using Xunit;
 
 namespace Jeebs.Data.Querying.QueryOptions_Tests
 {
-	public class GetParts_Tests
+	public class GetParts_Tests : GetParts<TestOptions, TestId>
 	{
-		[Fact]
-		public void Adds_Id()
-		{
-			// Arrange
-			var id = F.Rnd.Lng;
-			var (table, _, idColumn, _, options) = QueryOptions_Setup.Get(opt => opt with { Id = new(id) });
-			var cols = Substitute.For<IColumnList>();
-
-			// Act
-			var result = options.GetPartsTest(table, cols, idColumn);
-
-			// Assert
-			var some = result.AssertSome();
-			Assert.Collection(some.Where,
-				x =>
-				{
-					Assert.Same(idColumn, x.column);
-					Assert.Equal(Compare.Equal, x.cmp);
-					Assert.Equal(id, x.value);
-				}
-			);
-		}
+		protected override TestOptions Create(IMapper mapper) =>
+			new(mapper);
 
 		[Fact]
-		public void Adds_Ids()
-		{
-			// Arrange
-			var i0 = F.Rnd.Lng;
-			var i1 = F.Rnd.Lng;
-			var ids = new QueryOptions_Setup.TestId[] { new(i0), new(i1) };
-			var (table, _, idColumn, _, options) = QueryOptions_Setup.Get(opt => opt with { Ids = ids });
-			var cols = Substitute.For<IColumnList>();
-
-			// Act
-			var result = options.GetPartsTest(table, cols, idColumn);
-
-			// Assert
-			var some = result.AssertSome();
-			Assert.Collection(some.Where,
-				x =>
-				{
-					Assert.Same(idColumn, x.column);
-					Assert.Equal(Compare.In, x.cmp);
-
-					var value = Assert.IsAssignableFrom<IEnumerable<long>>(x.value);
-					Assert.Collection(value,
-						y => Assert.Equal(i0, y),
-						y => Assert.Equal(i1, y)
-					);
-				}
-			);
-		}
+		public override void Test00_Adds_Id() =>
+			Test00();
 
 		[Fact]
-		public void Adds_SortRandom()
-		{
-			// Arrange
-			var (table, _, idColumn, _, options) = QueryOptions_Setup.Get(opt => opt with { SortRandom = true });
-			var cols = Substitute.For<IColumnList>();
-
-			// Act
-			var result = options.GetPartsTest(table, cols, idColumn);
-
-			// Assert
-			var some = result.AssertSome();
-			Assert.True(some.SortRandom);
-		}
+		public override void Test01_Adds_Ids() =>
+			Test01();
 
 		[Fact]
-		public void Adds_Sort()
-		{
-			// Arrange
-			var c0 = Substitute.For<IColumn>();
-			var o0 = SortOrder.Ascending;
-			var c1 = Substitute.For<IColumn>();
-			var o1 = SortOrder.Descending;
-			var sort = new[] { (c0, o0), (c1, o1) };
-			var (table, _, idColumn, _, options) = QueryOptions_Setup.Get(opt => opt with { Sort = sort });
-			var cols = Substitute.For<IColumnList>();
+		public override void Test02_Adds_SortRandom() =>
+			Test02();
 
-			// Act
-			var result = options.GetPartsTest(table, cols, idColumn);
-
-			// Assert
-			var some = result.AssertSome();
-			Assert.Collection(some.Sort,
-				x =>
-				{
-					Assert.Same(c0, x.column);
-					Assert.Equal(o0, x.order);
-				},
-				x =>
-				{
-					Assert.Same(c1, x.column);
-					Assert.Equal(o1, x.order);
-				}
-			);
-		}
+		[Fact]
+		public override void Test03_Adds_Sort() =>
+			Test03();
 	}
 }
