d09647ac710c64fff0aae91ed7a40e6a4b472994
Ben Green <ben@bcgdesign.com>
2021-05-18 09:31:50 +0100

    Adding additional launch settings to Api project

--- apps/AppApi/App.cs
+++ apps/AppApi/App.cs
@@ -1,6 +1,18 @@
 ﻿// Jeebs Test Applications
 // Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
 
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace AppApi
 {
 	public sealed class App : Jeebs.Apps.ApiApp
