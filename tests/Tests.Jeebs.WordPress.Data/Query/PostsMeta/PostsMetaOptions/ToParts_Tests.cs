023189dd5328ef297b1d7e6a37d8d2aa88d5e30e
Ben Green <ben@bcgdesign.com>
2021-05-19 07:28:14 +0100

    Adding PostMetaOptions tests

--- tests/Tests.Jeebs.WordPress.Data/Query/PostsMeta/PostsMetaOptions/ToParts_Tests.cs
+++ tests/Tests.Jeebs.WordPress.Data/Query/PostsMeta/PostsMetaOptions/ToParts_Tests.cs
@@ -1,8 +1,10 @@
 ﻿// Jeebs Unit Tests
 // Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
 
+using Jeebs.Data.Querying;
 using Jeebs.Data.Querying.QueryOptions_Tests;
 using Jeebs.WordPress.Data.Entities;
+using NSubstitute;
 using Xunit;
 
 namespace Jeebs.WordPress.Data.Query_Tests.PostsMetaOptions_Tests
@@ -16,6 +18,88 @@ namespace Jeebs.WordPress.Data.Query_Tests.PostsMetaOptions_Tests
 			var options = new Query.PostsMetaOptions(schema, builder);
 
 			return (options, builder);
+		}
+
+		[Fact]
+		public void PostId_Null_PostIds_Empty_Does_Not_Call_Builder_AddWherePostId()
+		{
+			// Arrange
+			var (options, builder) = Setup();
+
+			// Act
+			options.ToParts<TestModel>();
+
+			// Assert
+			builder.DidNotReceiveWithAnyArgs().AddWherePostId(Arg.Any<QueryParts>(), default, Arg.Any<IImmutableList<WpPostId>>());
+		}
+
+		[Fact]
+		public void PostId_Not_Null_Calls_Builder_AddWherePostId()
+		{
+			// Arrange
+			var (options, builder) = Setup();
+			var postId = new WpPostId(F.Rnd.Lng);
+			var opt = options with
+			{
+				PostId = postId
+			};
+
+			// Act
+			opt.ToParts<TestModel>();
+
+			// Assert
+			builder.Received().AddWherePostId(Arg.Any<QueryParts>(), postId, Arg.Any<IImmutableList<WpPostId>>());
+		}
+
+		[Fact]
+		public void PostIds_Not_Empty_Calls_Builder_AddWherePostId()
+		{
+			// Arrange
+			var (options, builder) = Setup();
+			var i0 = new WpPostId(F.Rnd.Lng);
+			var i1 = new WpPostId(F.Rnd.Lng);
+			var postIds = ImmutableList.Create(i0, i1);
+			var opt = options with
+			{
+				PostIds = postIds
+			};
+
+			// Act
+			opt.ToParts<TestModel>();
+
+			// Assert
+			builder.Received().AddWherePostId(Arg.Any<QueryParts>(), null, postIds);
+		}
+
+		[Fact]
+		public void Key_Null_Does_Not_Call_Builder_AddWhereKey()
+		{
+			// Arrange
+			var (options, builder) = Setup();
+
+			// Act
+			options.ToParts<TestModel>();
+
+			// Assert
+			builder.DidNotReceiveWithAnyArgs().AddWhereKey(Arg.Any<QueryParts>(), default);
+		}
+
+		[Fact]
+		public void Key_Not_Null_Calls_Builder_AddWhereKey()
+		{
+			// Arrange
+			var (options, builder) = Setup();
+			var key = F.Rnd.Str;
+			var opt = options with
+			{
+				Key = key
+			};
+
+			// Act
+			opt.ToParts<TestModel>();
+
+			// Assert
+			builder.Received().AddWhereKey(Arg.Any<QueryParts>(), key);
 		}
 
 		[Fact]
