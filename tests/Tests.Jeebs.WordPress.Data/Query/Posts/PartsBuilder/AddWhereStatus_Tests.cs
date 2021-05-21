3adcbba3dc0da16e0fea47b5d74a342706e8a04c
Ben Green <ben@bcgdesign.com>
2021-05-21 09:46:49 +0100

    Adding AssertWhere and AddWhere tests to QueryPosts PartsBuilder

--- tests/Tests.Jeebs.WordPress.Data/Query/Posts/PartsBuilder/AddWhereStatus_Tests.cs
+++ tests/Tests.Jeebs.WordPress.Data/Query/Posts/PartsBuilder/AddWhereStatus_Tests.cs
@@ -11,23 +11,23 @@ using static Jeebs.WordPress.Data.Query_Tests.PostsPartsBuilder_Tests.Setup;
 
 namespace Jeebs.WordPress.Data.Query_Tests.PostsPartsBuilder_Tests
 {
-	public class AddWhereType_Tests : QueryPartsBuilder_Tests<Query.PostsPartsBuilder, WpPostId>
+	public class AddWhereStatus_Tests : QueryPartsBuilder_Tests<Query.PostsPartsBuilder, WpPostId>
 	{
 		protected override Query.PostsPartsBuilder GetConfiguredBuilder(IExtract extract) =>
 			GetBuilder(extract);
 
 		[Fact]
-		public void Adds_Type_To_Where()
+		public void Adds_Status_To_Where()
 		{
 			// Arrange
 			var (builder, v) = Setup();
-			var type = new PostType(F.Rnd.Str);
+			var status = new PostStatus(F.Rnd.Str);
 
 			// Act
-			var result = builder.AddWhereType(v.Parts, type);
+			var result = builder.AddWhereStatus(v.Parts, status);
 
 			// Assert
-			AssertWhere(v.Parts, result, Post.Type, Compare.Equal, type);
+			AssertWhere(v.Parts, result, Post.Status, Compare.Equal, status);
 		}
 	}
 }
--- tests/Tests.Jeebs.WordPress.Data/Query/Posts/PartsBuilder/_Setup.cs
+++ tests/Tests.Jeebs.WordPress.Data/Query/Posts/PartsBuilder/_Setup.cs
@@ -2,15 +2,37 @@
 // Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
 
 using Jeebs.Data;
+using Jeebs.Data.Enums;
+using Jeebs.Data.Querying;
+using Jeebs.WordPress.Data.Tables;
+using Xunit;
 
 namespace Jeebs.WordPress.Data.Query_Tests.PostsPartsBuilder_Tests
 {
 	public static class Setup
 	{
-		public static Query.PostsPartsBuilder GetBuilder(IExtract extract)
+		private readonly static WpDbSchema schema =
+			new(F.Rnd.Str);
+
+		public static PostTable Post { get; } =
+			schema.Post;
+
+		public static Query.PostsPartsBuilder GetBuilder(IExtract extract) =>
+			new(extract, schema);
+
+		public static void AssertWhere(QueryParts parts, Option<QueryParts> result, string column, Compare cmp, object value)
 		{
-			var schema = new WpDbSchema(F.Rnd.Str);
-			return new(extract, schema);
+			var some = result.AssertSome();
+			Assert.NotSame(parts, some);
+			Assert.Collection(some.Where,
+				x =>
+				{
+					Assert.Equal(Post.GetName(), x.column.Table);
+					Assert.Equal(column, x.column.Name);
+					Assert.Equal(cmp, x.cmp);
+					Assert.Equal(value, x.value);
+				}
+			);
 		}
 	}
 }
