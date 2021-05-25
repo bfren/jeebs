f955edc12705482473e2b546bc0683b7fda8fdd1
Ben Green <ben@bcgdesign.com>
2021-05-25 09:57:44 +0100

    Add Where ParentId

---
+++ tests/Tests.Jeebs.WordPress.Data/Query/Posts/PartsBuilder/AddWhereParentId_Tests.cs
@@ -0,0 +1,49 @@
+﻿// Jeebs Unit Tests
+// Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
+
+using Jeebs.Data;
+using Jeebs.Data.Enums;
+using Jeebs.Data.Querying.QueryPartsBuilder_Tests;
+using Jeebs.WordPress.Data.Entities;
+using Xunit;
+using static Jeebs.WordPress.Data.Query_Tests.PostsPartsBuilder_Tests.Setup;
+
+namespace Jeebs.WordPress.Data.Query_Tests.PostsPartsBuilder_Tests
+{
+	public class AddWhereParentId_Tests : QueryPartsBuilder_Tests<Query.PostsPartsBuilder, WpPostId>
+	{
+		protected override Query.PostsPartsBuilder GetConfiguredBuilder(IExtract extract) =>
+			GetBuilder(extract);
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData(0)]
+		[InlineData(-1)]
+		public void Invalid_ParentId_Does_Nothing(long? input)
+		{
+			// Arrange
+			var (builder, v) = Setup();
+
+			// Act
+			var result = builder.AddWhereParentId(v.Parts, input);
+
+			// Assert
+			var some = result.AssertSome();
+			Assert.Same(v.Parts, some);
+		}
+
+		[Fact]
+		public void Adds_ParentId_Equal()
+		{
+			// Arrange
+			var (builder, v) = Setup();
+			var parentId = F.Rnd.NumberF.GetInt64(1, max: 1000);
+
+			// Act
+			var result = builder.AddWhereParentId(v.Parts, parentId);
+
+			// Assert
+			AssertWhere(v.Parts, result, Post.ParentId, Compare.Equal, parentId);
+		}
+	}
+}
