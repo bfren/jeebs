34301bcf880b62bb4e78f56bf705109b803fa154
Ben Green <ben@bcgdesign.com>
2021-05-25 13:17:56 +0100

    Adding Where PostId tests

---
+++ tests/Tests.Jeebs.WordPress.Data/Query/PostsMeta/PartsBuilder/_Setup.cs
@@ -0,0 +1,41 @@
+﻿// Jeebs Unit Tests
+// Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
+
+using Jeebs.Data;
+using Jeebs.Data.Enums;
+using Jeebs.Data.Querying;
+using Jeebs.WordPress.Data.Tables;
+using Xunit;
+
+namespace Jeebs.WordPress.Data.Query_Tests.PostsMetaPartsBuilder_Tests
+{
+	public static class Setup
+	{
+		private readonly static WpDbSchema schema =
+			new(F.Rnd.Str);
+
+		public static PostTable Post { get; } =
+			schema.Post;
+
+		public static PostMetaTable PostMeta { get; } =
+			schema.PostMeta;
+
+		public static Query.PostsMetaPartsBuilder GetBuilder(IExtract extract) =>
+			new(extract, schema);
+
+		public static void AssertWhere(QueryParts parts, Option<QueryParts> result, string column, Compare cmp, object value)
+		{
+			var some = result.AssertSome();
+			Assert.NotSame(parts, some);
+			Assert.Collection(some.Where,
+				x =>
+				{
+					Assert.Equal(PostMeta.GetName(), x.column.Table);
+					Assert.Equal(column, x.column.Name);
+					Assert.Equal(cmp, x.cmp);
+					Assert.Equal(value, x.value);
+				}
+			);
+		}
+	}
+}
