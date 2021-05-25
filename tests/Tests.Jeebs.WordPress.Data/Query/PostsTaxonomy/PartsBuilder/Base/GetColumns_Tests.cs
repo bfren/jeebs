1ba26fb76580619877bf5a1735fa9418c279c58d
Ben Green <ben@bcgdesign.com>
2021-05-25 13:30:52 +0100

    Update GetColumns_Tests.cs

--- tests/Tests.Jeebs.WordPress.Data/Query/PostsTaxonomy/PartsBuilder/Base/GetColumns_Tests.cs
+++ tests/Tests.Jeebs.WordPress.Data/Query/PostsTaxonomy/PartsBuilder/Base/GetColumns_Tests.cs
@@ -4,6 +4,8 @@
 using Jeebs.Data;
 using Jeebs.Data.Querying.QueryPartsBuilder_Tests;
 using Jeebs.WordPress.Data.Entities;
+using Jeebs.WordPress.Data.Tables;
+using NSubstitute;
 using Xunit;
 using static Jeebs.WordPress.Data.Query_Tests.PostsTaxonomyPartsBuilder_Tests.Setup;
 
@@ -13,6 +15,23 @@ namespace Jeebs.WordPress.Data.Query_Tests.PostsTaxonomyPartsBuilder_Tests
 	{
 		protected override Query.PostsTaxonomyPartsBuilder GetConfiguredBuilder(IExtract extract) =>
 			GetBuilder(extract);
+
+		[Fact]
+		public void Calls_Extract_From_With_Tables()
+		{
+			// Arrange
+			var (builder, v) = Setup();
+
+			// Act
+			builder.GetColumns<WpPostEntity>();
+
+			// Assert
+			v.Extract.Received().From<WpPostEntity>(
+				Arg.Any<TermTable>(),
+				Arg.Any<TermRelationshipTable>(),
+				Arg.Any<TermTaxonomyTable>()
+			);
+		}
 
 		[Fact]
 		public override void Test00_Calls_Extract_From() =>
