6dc130c7ffd0c0fa9ff7719da3caa4470d77ac7e
Ben Green <ben@bcgdesign.com>
2021-05-25 13:58:42 +0100

    Update AddSort_Tests.cs

--- tests/Tests.Jeebs.WordPress.Data/Query/PostsTaxonomy/PartsBuilder/Base/AddSort_Tests.cs
+++ tests/Tests.Jeebs.WordPress.Data/Query/PostsTaxonomy/PartsBuilder/Base/AddSort_Tests.cs
@@ -2,8 +2,13 @@
 // Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
 
 using Jeebs.Data;
+using Jeebs.Data.Enums;
+using Jeebs.Data.Mapping;
 using Jeebs.Data.Querying.QueryPartsBuilder_Tests;
 using Jeebs.WordPress.Data.Entities;
+using Jeebs.WordPress.Data.Enums;
+using Jeebs.WordPress.Data.Tables;
+using NSubstitute;
 using Xunit;
 using static Jeebs.WordPress.Data.Query_Tests.PostsTaxonomyPartsBuilder_Tests.Setup;
 
@@ -13,6 +18,62 @@ namespace Jeebs.WordPress.Data.Query_Tests.PostsTaxonomyPartsBuilder_Tests
 	{
 		protected override Query.PostsTaxonomyPartsBuilder GetConfiguredBuilder(IExtract extract) =>
 			GetBuilder(extract);
+
+		[Fact]
+		public void SortBy_TitleAscending_Adds_Sort_Title_Ascending()
+		{
+			// Arrange
+			var (builder, v) = Setup();
+			var sortBy = TaxonomySort.TitleAscending;
+
+			// Act
+			var result = builder.AddSort(v.Parts, false, Substitute.For<IImmutableList<(IColumn, SortOrder)>>(), sortBy);
+
+			// Assert
+			var some = result.AssertSome();
+			Assert.NotSame(v.Parts, some);
+			Assert.Collection(some.Sort,
+				x =>
+				{
+					Assert.Equal(builder.TTest.Term.GetName(), x.column.Table);
+					Assert.Equal(builder.TTest.Term.Title, x.column.Name);
+					Assert.Equal(nameof(TermTable.Title), x.column.Alias);
+					Assert.Equal(SortOrder.Ascending, x.order);
+				}
+			);
+		}
+
+		[Fact]
+		public void SortBy_CountDescending_Adds_Sort_Count_Descending_Title_Ascending()
+		{
+			// Arrange
+			var (builder, v) = Setup();
+			var sortBy = TaxonomySort.CountDescending;
+
+			// Act
+			var result = builder.AddSort(v.Parts, false, Substitute.For<IImmutableList<(IColumn, SortOrder)>>(), sortBy);
+
+			// Assert
+			var some = result.AssertSome();
+			Assert.NotSame(v.Parts, some);
+			Assert.Collection(some.Sort,
+				x =>
+				{
+					Assert.Equal(builder.TTest.TermTaxonomy.GetName(), x.column.Table);
+					Assert.Equal(builder.TTest.TermTaxonomy.Count, x.column.Name);
+					Assert.Equal(nameof(TermTaxonomyTable.Count), x.column.Alias);
+					Assert.Equal(SortOrder.Descending, x.order);
+				},
+				x =>
+				{
+					Assert.Equal(builder.TTest.Term.GetName(), x.column.Table);
+					Assert.Equal(builder.TTest.Term.Title, x.column.Name);
+					Assert.Equal(nameof(TermTable.Title), x.column.Alias);
+					Assert.Equal(SortOrder.Ascending, x.order);
+				}
+			);
+		}
+
 
 		[Fact]
 		public override void Test00_SortRandom_True_Returns_New_Parts_With_SortRandom_True() =>
