942100dfcb06326bc3ce72eec744aa5166728e82
Ben Green <ben@bcgdesign.com>
2021-05-25 14:04:26 +0100

    Moving tests to main directory

--- tests/Tests.Jeebs.WordPress.Data/Query/Terms/PartsBuilder/GetColumns_Tests.cs
+++ tests/Tests.Jeebs.WordPress.Data/Query/Terms/PartsBuilder/GetColumns_Tests.cs
@@ -4,6 +4,8 @@
 using Jeebs.Data;
 using Jeebs.Data.Querying.QueryPartsBuilder_Tests;
 using Jeebs.WordPress.Data.Entities;
+using Jeebs.WordPress.Data.Tables;
+using NSubstitute;
 using Xunit;
 using static Jeebs.WordPress.Data.Query_Tests.TermsPartsBuilder_Tests.Setup;
 
@@ -13,6 +15,22 @@ namespace Jeebs.WordPress.Data.Query_Tests.TermsPartsBuilder_Tests
 	{
 		protected override Query.TermsPartsBuilder GetConfiguredBuilder(IExtract extract) =>
 			GetBuilder(extract);
+
+		[Fact]
+		public void Calls_Extract_From_With_Tables()
+		{
+			// Arrange
+			var (builder, v) = Setup();
+
+			// Act
+			builder.GetColumns<WpPostEntity>();
+
+			// Assert
+			v.Extract.Received().From<WpPostEntity>(
+				Arg.Any<TermTable>(),
+				Arg.Any<TermTaxonomyTable>()
+			);
+		}
 
 		[Fact]
 		public override void Test00_Calls_Extract_From() =>
