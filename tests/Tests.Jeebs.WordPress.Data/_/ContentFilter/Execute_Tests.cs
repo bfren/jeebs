734199bff73da3726c13e26b40226b5621c13c8f
Ben Green <ben@bcgdesign.com>
2021-05-28 15:11:06 +0100

    Create Execute_Tests.cs

---
+++ tests/Tests.Jeebs.WordPress.Data/_/ContentFilter/Execute_Tests.cs
@@ -0,0 +1,28 @@
+﻿// Jeebs Unit Tests
+// Copyright (c) bcg|design - licensed under https://mit.bcgdesign.com/2013
+
+using System;
+using NSubstitute;
+using Xunit;
+
+namespace Jeebs.WordPress.Data.ContentFilter_Tests
+{
+	public class Execute_Tests
+	{
+		[Fact]
+		public void Runs_Filter()
+		{
+			// Arrange
+			var filter = Substitute.For<Func<string, string>>();
+			var content = F.Rnd.Str;
+
+			var contentFilter = Substitute.ForPartsOf<ContentFilter>(filter);
+
+			// Act
+			_ = contentFilter.Execute(content);
+
+			// Assert
+			filter.Received().Invoke(content);
+		}
+	}
+}
