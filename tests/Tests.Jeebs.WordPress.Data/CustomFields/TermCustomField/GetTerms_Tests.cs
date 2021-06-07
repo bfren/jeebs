c1cb62d543a916fa95402064987613843f4edf81
Ben Green <ben@bcgdesign.com>
2021-06-07 08:10:39 +0100

    Adding remaining TermCustomField tests

---
+++ tests/Tests.Jeebs.WordPress.Data/CustomFields/TermCustomField/GetTerms_Tests.cs
@@ -0,0 +1,29 @@
+﻿// Jeebs Unit Tests
+// Copyright (c) bfren.uk - licensed under https://mit.bfren.uk/2013
+
+using NSubstitute;
+using Xunit;
+using static Jeebs.WordPress.Data.TermCustomField;
+
+namespace Jeebs.WordPress.Data.CustomFields.TermCustomField_Tests
+{
+	public class GetTerms_Tests
+	{
+		[Fact]
+		public void Calls_Db_Query_TermsAsync_With_TermId()
+		{
+			// Arrange
+			var query = Substitute.For<IWpDbQuery>();
+			var schema = Substitute.For<IWpDbSchema>();
+			var db = Substitute.For<IWpDb>();
+			db.Query.Returns(query);
+			var id = F.Rnd.Lng;
+
+			// Act
+			_ = GetTerms(db, new(id));
+
+			// Assert
+			db.Query.Received().TermsAsync<Term>(Arg.Is<Query.GetTermsOptions>(opt => opt(new(schema)).Id! == id));
+		}
+	}
+}
--- tests/Tests.Jeebs.WordPress.Data/CustomFields/TermCustomField/HydrateAsync_Tests.cs
+++ tests/Tests.Jeebs.WordPress.Data/CustomFields/TermCustomField/HydrateAsync_Tests.cs
@@ -4,6 +4,7 @@
 using System.Threading.Tasks;
 using NSubstitute;
 using Xunit;
+using static Jeebs.WordPress.Data.TermCustomField;
 using static Jeebs.WordPress.Data.TermCustomField.Msg;
 
 namespace Jeebs.WordPress.Data.CustomFields.TermCustomField_Tests
@@ -42,6 +43,61 @@ namespace Jeebs.WordPress.Data.CustomFields.TermCustomField_Tests
 
 			// Assert
 			result.AssertFalse();
+		}
+
+		[Fact]
+		public async Task Meta_Contains_Key_Multiple_Terms_Found_Returns_None_With_MultipleTermsFoundMsg()
+		{
+			// Arrange
+			var t0 = new Term();
+			var t1 = new Term();
+			var terms = new[] { t0, t1 };
+
+			var query = Substitute.For<IWpDbQuery>();
+			query.TermsAsync<Term>(Arg.Any<Query.GetTermsOptions>()).Returns(terms);
+
+			var db = Substitute.For<IWpDb>();
+			db.Query.Returns(query);
+
+			var key = F.Rnd.Str;
+			var value = F.Rnd.Lng;
+			var meta = new MetaDictionary { { key, value.ToString() } };
+			var field = new Test(key);
+
+			// Act
+			var result = await field.HydrateAsync(db, meta, true);
+
+			// Assert
+			var none = result.AssertNone();
+			var msg = Assert.IsType<MultipleTermsFoundMsg>(none);
+			Assert.Equal(value.ToString(), msg.Value);
+		}
+
+		[Fact]
+		public async Task Meta_Contains_Key_Single_Item_Found_Sets_ValueObj_Returns_True()
+		{
+			// Arrange
+			var term = new Term();
+			var terms = new[] { term };
+
+			var query = Substitute.For<IWpDbQuery>();
+			query.TermsAsync<Term>(Arg.Any<Query.GetTermsOptions>()).Returns(terms);
+
+			var db = Substitute.For<IWpDb>();
+			db.Query.Returns(query);
+
+			var key = F.Rnd.Str;
+			var value = F.Rnd.Lng;
+			var meta = new MetaDictionary { { key, value.ToString() } };
+			var field = new Test(key);
+
+			// Act
+			var result = await field.HydrateAsync(db, meta, true);
+
+			// Assert
+			result.AssertTrue();
+			Assert.Same(term, field.ValueObj);
+
 		}
 
 		public record Test(string Key) : TermCustomField(Key);
