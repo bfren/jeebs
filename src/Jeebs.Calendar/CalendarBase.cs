77be2bd7b1bb8e32ca5a7bf88ba8afe936c0e420
Ben Green <ben@bcgdesign.com>
2021-08-19 09:45:36 +0100

    Adding base calendar for shared functionality - see #62

---
+++ src/Jeebs.Calendar/CalendarBase.cs
@@ -0,0 +1,36 @@
+﻿// Jeebs Rapid Application Development
+// Copyright (c) bfren.uk - licensed under https://mit.bfren.uk/2013
+
+using Jeebs.Calendar.Models;
+
+namespace Jeebs.Calendar
+{
+	/// <summary>
+	/// Provides base calendar functionality
+	/// </summary>
+	public abstract class CalendarBase
+	{
+		/// <summary>
+		/// The default time zone, only used if none is specified
+		/// </summary>
+		public const string DefaultTimeZone = "Europe/London";
+
+		/// <summary>
+		/// The calendar
+		/// </summary>
+		internal readonly CalendarModel calendar;
+
+		/// <summary>
+		/// The calendar's timezone (default: see <see cref="DefaultTimeZone"/>)
+		/// </summary>
+		internal readonly string tzid;
+
+		/// <summary>
+		/// Create object
+		/// </summary>
+		/// <param name="calendar">Calendar</param>
+		/// <param name="tzid">Timezone</param>
+		protected CalendarBase(CalendarModel calendar, string tzid) =>
+			(this.calendar, this.tzid) = (calendar, tzid);
+	}
+}
