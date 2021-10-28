376740632dc5bd9e1caf3621d2e140fd1d89109f
Ben Green <ben@bcgdesign.com>
2021-10-28 12:11:37 +0100

    Adding PostgreSQL console test app - see #92

---
+++ apps/AppConsolePg/App.cs
@@ -0,0 +1,8 @@
+﻿// Jeebs Test Applications
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
+
+namespace AppConsolePg;
+
+internal sealed class App : Jeebs.Apps.ConsoleApp
+{
+}
---
+++ apps/AppConsolePg/Program.cs
@@ -0,0 +1,11 @@
+﻿// Jeebs Test Applications
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
+
+using AppConsolePg;
+
+await Jeebs.Apps.Program.MainAsync<App>(args, async (provider, log) =>
+{
+	// Begin
+	log.Debug("= PostgreSQL Console Test =");
+
+});
