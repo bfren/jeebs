59ce7e38a971d26728e35fd1c1bbe6c31e50d9c1
Ben Green <ben@bcgdesign.com>
2021-10-28 16:25:24 +0100

    Re-adding PostgreSql directory

---
+++ src/Jeebs.Data.Clients.PostgreSql/PostgreSqlDbClient.cs
@@ -0,0 +1,74 @@
+﻿// Jeebs Rapid Application Development
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
+
+using System.Collections.Generic;
+using System.Data;
+using Jeebs.Data.Enums;
+using Jeebs.Data.Mapping;
+using Npgsql;
+
+namespace Jeebs.Data.Clients.PostgreSql;
+
+/// <inheritdoc cref="IDbClient"/>
+public partial class PostgreSqlDbClient : DbClient
+{
+	/// <inheritdoc/>
+	public override IDbConnection Connect(string connectionString) =>
+		new NpgsqlConnection(connectionString);
+
+	/// <inheritdoc/>
+	public override string Escape(IColumn column, bool withAlias = false) =>
+		withAlias switch
+		{
+			true =>
+				Escape(column.Name) + $" AS \"{column.Alias}\"",
+
+			false =>
+				Escape(column.Name)
+		};
+
+	/// <inheritdoc/>
+	public override string EscapeWithTable(IColumn column, bool withAlias = false) =>
+		withAlias switch
+		{
+			true =>
+				Escape(column.Name, column.Table) + $" AS \"{column.Alias}\"",
+
+			false =>
+				Escape(column.Name, column.Table)
+		};
+
+	/// <inheritdoc/>
+	public override string Escape(ITable table) =>
+		Escape(table.GetName());
+
+	/// <inheritdoc/>
+	public override string Escape(string columnOrTable) =>
+		$"\"{columnOrTable}\"";
+
+	/// <inheritdoc/>
+	public override string Escape(string column, string table) =>
+		Escape(table) + "." + Escape(column);
+
+	/// <inheritdoc/>
+	public override string GetOperator(Compare cmp) =>
+		cmp.ToOperator();
+
+	/// <inheritdoc/>
+	public override string GetParamRef(string paramName) =>
+		$"@{paramName}";
+
+	/// <inheritdoc/>
+	public override string JoinList(List<string> objects, bool wrap)
+	{
+		var list = string.Join(", ", objects);
+		return wrap switch
+		{
+			true =>
+				$"({list})",
+
+			false =>
+				list
+		};
+	}
+}
