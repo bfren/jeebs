9630ad2d48d6ef3520156933b90dbe61a62969b4
Ben Green <ben@bcgdesign.com>
2021-10-28 15:18:16 +0100

    Adding create / drop table tests

--- apps/AppConsolePg/Program.cs
+++ apps/AppConsolePg/Program.cs
@@ -13,14 +13,31 @@ await Jeebs.Apps.Program.MainAsync<App>(args, async (provider, log) =>
 
 	// Get services
 	var db = provider.GetRequiredService<Db>();
+	Console.WriteLine();
+
+	// Create table
+	log.Debug("== Creating table ==");
+	const string table = "\"console\".\"test\"";
+	await db
+		.ExecuteAsync(
+			$"CREATE TABLE IF NOT EXISTS {table} " +
+			"(" +
+			"\"id\" integer NOT NULL GENERATED ALWAYS AS IDENTITY, " +
+			"\"foo\" text NOT NULL, " +
+			"\"bar\" character(6)" +
+			");",
+			null,
+			CommandType.Text
+		);
+	Console.WriteLine();
 
 	// Insert into the table
-	log.Debug("== Inserting data ==");
+	log.Debug(" == Inserting data ==");
 	var foo = F.Rnd.Str;
 	var bar = F.Rnd.Str;
 	await db
 		.ExecuteAsync(
-			"INSERT INTO \"console\".\"test\" (\"foo\", \"bar\") VALUES (@foo, @bar);", new { foo, bar }, CommandType.Text
+			$"INSERT INTO {table} (\"foo\", \"bar\") VALUES (@foo, @bar);", new { foo, bar }, CommandType.Text
 		)
 		.AuditAsync(
 			none: r => log.Message(r)
@@ -32,14 +49,10 @@ await Jeebs.Apps.Program.MainAsync<App>(args, async (provider, log) =>
 	var id = 0;
 	await db
 		.QuerySingleAsync<Test>(
-			"SELECT * FROM \"console\".\"test\" WHERE \"foo\" = @foo AND \"bar\" = @bar;", new { foo, bar }, CommandType.Text
+			$"SELECT * FROM {table} WHERE \"foo\" = @foo AND \"bar\" = @bar;", new { foo, bar }, CommandType.Text
 		)
 		.AuditAsync(
-			some: x =>
-			{
-				log.Debug("Found row: {@Row}", x);
-				id = x.Id;
-			},
+			some: x => { if (x.Foo == foo) { log.Debug("Succeeded: {@Test}.", x); id = x.Id; } else { log.Error("Failed."); } },
 			none: r => log.Message(r)
 		);
 	Console.WriteLine();
@@ -49,7 +62,7 @@ await Jeebs.Apps.Program.MainAsync<App>(args, async (provider, log) =>
 	var newFoo = F.Rnd.Str;
 	await db
 		.ExecuteAsync(
-			"UPDATE \"console\".\"test\" SET \"foo\" = @newFoo WHERE \"id\" = @id;", new { newFoo, id }, CommandType.Text
+			$"UPDATE {table} SET \"foo\" = @newFoo WHERE \"id\" = @id;", new { newFoo, id }, CommandType.Text
 		)
 		.AuditAsync(
 			none: r => log.Message(r)
@@ -57,22 +70,36 @@ await Jeebs.Apps.Program.MainAsync<App>(args, async (provider, log) =>
 
 	await db
 		.QuerySingleAsync<Test>(
-			"SELECT * FROM \"console\".\"test\" WHERE \"id\" = @id;", new { id }, CommandType.Text
+			$"SELECT * FROM {table} WHERE \"id\" = @id;", new { id }, CommandType.Text
 		)
 		.AuditAsync(
-			some: x =>
-			{
-				if (x.Foo == newFoo)
-				{
-					log.Debug("Succeeded.");
-				}
-				else
-				{
-					log.Error("Failed.");
-				}
-			},
+			some: x => { if (x.Foo == newFoo) { log.Debug("Succeeded: {@Test}.", x); } else { log.Error("Failed."); } },
 			none: r => log.Message(r)
 		);
+	Console.WriteLine();
+
+	// Delete data
+	log.Debug("== Deleting data ==");
+	await db
+		.ExecuteAsync(
+			$"DELETE FROM {table} WHERE \"id\" = @id;", new { id }, CommandType.Text
+		)
+		.AuditAsync(
+			some: x => { if (x) { log.Debug("Succeeded."); } else { log.Error("Failed."); } },
+			none: r => log.Message(r)
+		);
+	Console.WriteLine();
+
+	// Drop table
+	log.Debug("== Dropping table == ");
+	await db
+		.ExecuteAsync(
+			$"DROP TABLE {table};", null, CommandType.Text
+		);
+
+	// Done
+	log.Debug("Done.");
 });
 
 record struct Test(int Id, string Foo, string Bar);
+record struct Json(string Val);
