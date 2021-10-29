04358db5119a370a0b6a48b0f0bb82985a1fb002
Ben Green <ben@bcgdesign.com>
2021-10-29 10:40:16 +0100

    Fixing custom attributes tests

--- tests/Tests.Jeebs.Data.Clients.SqlServer/_/SqlServerDbClient/GetCreateQuery_Tests.cs
+++ tests/Tests.Jeebs.Data.Clients.SqlServer/_/SqlServerDbClient/GetCreateQuery_Tests.cs
@@ -1,9 +1,11 @@
 ﻿// Jeebs Unit Tests
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
 
+using System;
 using System.Reflection;
 using Jeebs.Data.Mapping;
 using NSubstitute;
+using NSubstitute.Extensions;
 using Xunit;
 
 namespace Jeebs.Data.Clients.SqlServer.SqlServerDbClient_Tests;
@@ -20,12 +22,14 @@ public class GetCreateQuery_Tests
 		var c0Alias = F.Rnd.Str;
 		var c0Property = Substitute.ForPartsOf<PropertyInfo>();
 		c0Property.Name.Returns(c0Alias);
+		c0Property.Configure().CustomAttributes.Returns(Array.Empty<CustomAttributeData>());
 		var c0 = new MappedColumn(table, c0Name, c0Property);
 
 		var c1Name = F.Rnd.Str;
 		var c1Alias = F.Rnd.Str;
 		var c1Property = Substitute.ForPartsOf<PropertyInfo>();
 		c1Property.Name.Returns(c1Alias);
+		c1Property.Configure().CustomAttributes.Returns(Array.Empty<CustomAttributeData>());
 		var c1 = new MappedColumn(table, c1Name, c1Property);
 
 		var list = new MappedColumnList(new[] { c0, c1 });
