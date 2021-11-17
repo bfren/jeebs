0a9982e00463cb11f4313a7eb1ae07db9b7b8458
Ben Green <ben@bcgdesign.com>
2021-11-17 15:05:19 +0000

    Adding LogLevel test for ExceptionMsg

--- tests/Tests.Jeebs.Types/Messages/ExceptionMsg/Level_Tests.cs
+++ tests/Tests.Jeebs.Types/Messages/ExceptionMsg/Level_Tests.cs
@@ -2,23 +2,24 @@
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
 
 using System;
+using Jeebs.Logging;
 using Xunit;
 
 namespace Jeebs.ExceptionMsg_Tests;
 
-public class Format_Tests
+public class Level_Tests
 {
 	[Fact]
-	public void Uses_Exception_As_Name()
+	public void Uses_LogLevel_Error()
 	{
 		// Arrange
 		var msg = new TestExceptionMsg(new());
 
 		// Act
-		var result = msg.Format;
+		var result = msg.Level;
 
 		// Assert
-		Assert.Equal("{{ Exception = {Value} }}", result);
+		Assert.Equal(LogLevel.Error, result);
 	}
 
 	public record class TestExceptionMsg(Exception Value) : ExceptionMsg;
