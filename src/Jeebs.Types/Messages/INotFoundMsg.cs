5c725b9e3c014a66c73fce8015863b98e08551d4
Ben Green <ben@bcgdesign.com>
2022-03-09 09:24:42 +0000

    Deleting Jeebs.Types - see #148

--- src/Jeebs.Types/Messages/INotFoundMsg.cs
+++
@@ -1,9 +0,0 @@
-﻿// Jeebs Rapid Application Development
-// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
-
-namespace Jeebs;
-
-/// <summary>
-/// Framework 'Not Found' message
-/// </summary>
-public interface INotFoundMsg { }
