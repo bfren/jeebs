dabbb5520d3132ce824dc9ef76fa15e96dad8c4c
Ben Green <ben@bcgdesign.com>
2022-03-15 10:24:53 +0000

    Making Crypto passphrases more secure - closes #162

--- src/Jeebs.Cryptography/Functions/CryptoF.GeneratePassphrase.cs
+++ src/Jeebs.Cryptography/Functions/CryptoF.GeneratePassphrase.cs
@@ -1,6 +1,7 @@
 // Jeebs Rapid Application Development
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
 
+using Jeebs.Messages;
 using MaybeF;
 using RndF;
 
@@ -9,15 +10,29 @@ namespace Jeebs.Cryptography.Functions;
 public static partial class CryptoF
 {
 	/// <summary>
-	/// Generate a 3-word passphrase
+	/// Generate an 8-word passphrase
 	/// </summary>
 	public static Maybe<string> GeneratePassphrase() =>
-		GeneratePassphrase(3);
+		Rnd.StringF.Passphrase();
 
 	/// <summary>
 	/// Generate a passphrase
 	/// </summary>
-	/// <param name="numberOfWords">The number of words in the passphrase (minimum: 2)</param>
+	/// <param name="numberOfWords">The number of words in the passphrase (minimum: 5)</param>
 	public static Maybe<string> GeneratePassphrase(int numberOfWords) =>
-		Rnd.StringF.Passphrase(numberOfWords);
+		numberOfWords switch
+		{
+			>= 5 =>
+				Rnd.StringF.Passphrase(numberOfWords),
+
+			_ =>
+				F.None<string>(new M.CryptographicallySecurePassphrasesMustContainAtLeastFiveWordsMsg(numberOfWords))
+		};
+
+	public static partial class M
+	{
+		/// <summary>EFF specify that with the long word list, at least five words must be used to be secure</summary>
+		/// <param name="Value">Number of words</param>
+		public sealed record class CryptographicallySecurePassphrasesMustContainAtLeastFiveWordsMsg(int Value) : WithValueMsg<int>;
+	}
 }
