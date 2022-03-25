62376328a22e7f05b387985d4d76f84b79c0cc39
Ben Green <ben@bcgdesign.com>
2022-03-25 12:52:42 +0000

    Adding handlers without Scrutor - #204

--- src/Jeebs.Cqrs/ServiceCollectionExtensions.cs
+++ src/Jeebs.Cqrs/ServiceCollectionExtensions.cs
@@ -2,6 +2,8 @@
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
 
 using System;
+using System.Linq;
+using Jeebs.Functions;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Jeebs.Cqrs;
@@ -22,26 +24,38 @@ public static class ServiceCollectionExtensions
 		_ = services.AddSingleton<ICommandDispatcher, CommandDispatcher>();
 		_ = services.AddSingleton<IQueryDispatcher, QueryDispatcher>();
 
-		// Get assemblies
-		var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-
-		// Add commands
-		_ = services.Scan(selector => selector
-			.FromAssemblies(assemblies)
-			.AddClasses(filter => filter.AssignableTo(typeof(ICommandHandler<>)))
-			.AsImplementedInterfaces()
-			.WithSingletonLifetime()
-		);
-
-		// Add queries
-		_ = services.Scan(selector => selector
-			.FromAssemblies(assemblies)
-			.AddClasses(filter => filter.AssignableTo(typeof(IQueryHandler<,>)))
-			.AsImplementedInterfaces()
-			.WithSingletonLifetime()
-		);
+		// Add handlers
+		AddHandlers(services, typeof(CommandHandler<>), 1);
+		AddHandlers(services, typeof(QueryHandler<,>), 2);
 
 		// Return
 		return services;
+	}
+
+	/// <summary>
+	/// Add handlers to <paramref name="services"/>
+	/// </summary>
+	/// <param name="services"></param>
+	/// <param name="handlerType">The handler base type</param>
+	/// <param name="genericArguments">The number of generic arguments the handler base type has</param>
+	internal static void AddHandlers(IServiceCollection services, Type handlerType, int genericArguments)
+	{
+		// Get all matching handlers
+		var handlers = from t in TypeF.AllTypes.Value
+					   where t.BaseType?.GenericTypeArguments.Length == genericArguments
+					   && t.BaseType.GetGenericTypeDefinition().IsAssignableFrom(handlerType)
+					   select t;
+
+		// Add each handler as itself, and as the base type so the dispatcher can find it
+		foreach (var q in handlers)
+		{
+			if (q.BaseType is null)
+			{
+				continue;
+			}
+
+			_ = services.AddSingleton(q);
+			_ = services.AddSingleton(q.BaseType, p => p.GetRequiredService(q));
+		}
 	}
 }
