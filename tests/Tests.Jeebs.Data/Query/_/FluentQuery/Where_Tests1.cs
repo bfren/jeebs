506aac1b5991ef5003407172b09f3dbbc9b08b5b
bfren <ben@bcgdesign.com>
2022-04-06 15:45:05 +0100

    Adding FluentQuery where DBNull tests - #264

--- tests/Tests.Jeebs.Data/Query/_/FluentQuery/Where_Tests1.cs
+++ tests/Tests.Jeebs.Data/Query/_/FluentQuery/Where_Tests1.cs
@@ -10,25 +10,6 @@ namespace Jeebs.Data.Query.FluentQuery_Tests;
 public class Where_Tests1 : FluentQuery_Tests
 {
 	[Fact]
-	public void Null_Value__Does_Not_Add_Predicate__Returns_Original_Query()
-	{
-		// Arrange
-		var (query, v) = Setup();
-
-		// Act
-		var r0 = query.Where(nameof(TestEntity.Foo), Compare.Like, null);
-		var r1 = query.Where(x => x.Foo, Compare.Like, null);
-
-		// Assert
-		var f0 = Assert.IsType<FluentQuery<TestEntity, TestId>>(r0);
-		Assert.Empty(f0.Parts.Where);
-		var f1 = Assert.IsType<FluentQuery<TestEntity, TestId>>(r1);
-		Assert.Empty(f1.Parts.Where);
-		Assert.Same(query, f0);
-		Assert.Same(query, f1);
-	}
-
-	[Fact]
 	public void Query_Errors__Does_Not_Add_Predicate__Returns_Original_Query()
 	{
 		// Arrange
@@ -49,6 +30,39 @@ public class Where_Tests1 : FluentQuery_Tests
 	}
 
 	[Theory]
+	[InlineData(Compare.Is)]
+	[InlineData(Compare.IsNot)]
+	public void Null_Value__Adds_Predicate_With_DBNull(Compare compare)
+	{
+		// Arrange
+		var (query, v) = Setup();
+
+		// Act
+		var r0 = query.Where(nameof(TestEntity.Foo), compare, null);
+		var r1 = query.Where(x => x.Foo, compare, null);
+
+		// Assert
+		var f0 = Assert.IsType<FluentQuery<TestEntity, TestId>>(r0);
+		Assert.Collection(f0.Parts.Where, x =>
+		{
+			Assert.Equal(v.Table.GetName(), x.column.TblName);
+			Assert.Equal(v.Table.Foo, x.column.ColName);
+			Assert.Equal(nameof(TestEntity.Foo), x.column.ColAlias);
+			Assert.Equal(compare, x.compare);
+			Assert.IsType<DBNull>(x.value);
+		});
+		var f1 = Assert.IsType<FluentQuery<TestEntity, TestId>>(r1);
+		Assert.Collection(f1.Parts.Where, x =>
+		{
+			Assert.Equal(v.Table.GetName(), x.column.TblName);
+			Assert.Equal(v.Table.Foo, x.column.ColName);
+			Assert.Equal(nameof(TestEntity.Foo), x.column.ColAlias);
+			Assert.Equal(compare, x.compare);
+			Assert.IsType<DBNull>(x.value);
+		});
+	}
+
+	[Theory]
 	[InlineData(Compare.Equal)]
 	[InlineData(Compare.NotEqual)]
 	[InlineData(Compare.Like)]
@@ -56,6 +70,8 @@ public class Where_Tests1 : FluentQuery_Tests
 	[InlineData(Compare.LessThanOrEqual)]
 	[InlineData(Compare.MoreThan)]
 	[InlineData(Compare.MoreThanOrEqual)]
+	[InlineData(Compare.Is)]
+	[InlineData(Compare.IsNot)]
 	public void Valid_Value__Adds_Predicate(Compare compare)
 	{
 		// Arrange
