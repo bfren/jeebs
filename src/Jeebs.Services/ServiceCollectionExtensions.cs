5f88bd87138dc053d3351a84e8392a2aa93cd4a7
bfren <ben@bcgdesign.com>
2022-04-08 16:30:04 +0100

    Using TypeF.AllTypes - #275

--- src/Jeebs.Services/ServiceCollectionExtensions.cs
+++ src/Jeebs.Services/ServiceCollectionExtensions.cs
@@ -4,8 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using Jeebs.Services.Extensions;
+using Jeebs.Functions;
 using Jeebs.Services.Notify;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -26,18 +25,8 @@ public static class ServiceCollectionExtensions
 		var foundListeners = false;
 		foreach (var t in GetImplementations(typeof(IDriver<>)))
 		{
+			// Register type
 			_ = @this.AddSingleton(t);
-
-			// Add required services
-			const BindingFlags flags = BindingFlags.NonPublic
-				| BindingFlags.Public
-				| BindingFlags.Static
-				| BindingFlags.FlattenHierarchy;
-
-			if (t.GetMethod("AddRequiredServices", flags) is MethodInfo addRequiredServices)
-			{
-				_ = addRequiredServices.Invoke(null, new object[] { @this });
-			}
 
 			// Add as a listener
 			if (t.Implements<INotificationListener>())
@@ -64,11 +53,8 @@ public static class ServiceCollectionExtensions
 
 		// Get implementations of an interface with a generic parameter
 		static IEnumerable<Type> GetImplementations(Type typeWithGenericParam) =>
-			from a in AppDomain.CurrentDomain.GetAssemblies()
-			from t in a.GetLoadableTypes()
-			from i in t.GetInterfaces()
-			where !t.IsAbstract && !t.IsInterface
-			&& i.IsGenericType && i.GetGenericTypeDefinition() == typeWithGenericParam
+			from t in TypeF.AllTypes.Value
+			where t.Implements(typeWithGenericParam) && !t.IsAbstract && !t.IsInterface
 			select t;
 	}
 }
