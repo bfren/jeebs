50bae1fda67f6f8fd8475761074d9f0f154a4689
bfren <ben@bcgdesign.com>
2022-04-13 08:17:16 +0100

    Adding AuthResult tests - #291

---
+++ tests/Tests.Jeebs.Mvc.Auth/_/AuthResult/TryAgain_Tests.cs
@@ -0,0 +1,27 @@
+// Jeebs Unit Tests
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
+
+using Jeebs.Mvc.Enums;
+using static Jeebs.Mvc.Auth.AuthResult.M;
+
+namespace Jeebs.Mvc.Auth.AuthResult_Tests;
+
+public class TryAgain_Tests
+{
+	[Fact]
+	public void Creates_With_Correct_Values()
+	{
+		// Arrange
+
+		// Act
+		var result = new AuthResult.TryAgain();
+
+		// Assert
+		Assert.False(result.Success);
+		Assert.Equal(AlertType.Error, result.Message.Type);
+		Assert.Equal(typeof(TryAgainMsg).ToString(), result.Message.Text);
+		Assert.Null(result.RedirectTo);
+		Assert.Equal(401, result.StatusCode);
+		Assert.False(result.Value);
+	}
+}
