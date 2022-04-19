a66c8c41af0a4b176c8174a2d81c43d279d2a4d2
bfren <ben@bcgdesign.com>
2022-04-19 12:31:19 +0100

    Adding ImmutableList jsonb type handler - #305

---
+++ src/Jeebs.Data.Clients.PostgreSql/TypeHandlers/ImmutableListJsonbTypeHandler.cs
@@ -0,0 +1,27 @@
+// Jeebs Rapid Application Development
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
+
+using Jeebs.Collections;
+
+namespace Jeebs.Data.Clients.PostgreSql.TypeHandlers;
+
+/// <summary>
+/// ImmutableList JsonTypeHandler
+/// </summary>
+/// <typeparam name="T">Enumerated type</typeparam>
+public sealed class ImmutableListJsonbTypeHandler<T> : JsonbTypeHandler<ImmutableList<T>>
+{
+	/// <summary>
+	/// Parse from list of string values and convert
+	/// </summary>
+	/// <param name="value">JSON string</param>
+	public override ImmutableList<T> Parse(object value) =>
+		value switch
+		{
+			string json =>
+				ImmutableList.Deserialise<T>(json),
+
+			_ =>
+				new()
+		};
+}
