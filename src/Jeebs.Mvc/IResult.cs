2d8677a0f0dab8ade3c19e501db26806ec5c86b2
bfren <ben@bcgdesign.com>
2022-04-22 16:36:57 +0100

    Adding IResult interface to separate properties - #319

--- src/Jeebs.Mvc/IResult.cs
+++ src/Jeebs.Mvc/IResult.cs
@@ -2,14 +2,29 @@
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
 
 using Jeebs.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Jeebs.Mvc;
 
 /// <summary>
-/// Represents the result of an operation
+/// Represents the result of an operation, and can be returned by MVC / Razor Page methods
 /// </summary>
+public interface IResult : IActionResult
+{
+	/// <summary>
+	/// HTTP status code - by default returns 200 on success or 500 on failure
+	/// </summary>
+	int StatusCode { get; }
+
+	/// <summary>
+	/// Optional URL to redirect to on success
+	/// </summary>
+	string? RedirectTo { get; }
+}
+
+/// <inheritdoc cref="IResult"/>
 /// <typeparam name="T">Value type</typeparam>
-public interface IResult<out T>
+public interface IResult<out T> : IResult
 {
 	/// <summary>
 	/// Returns true if the operation was a success
@@ -26,14 +41,4 @@ public interface IResult<out T>
 	/// Returns the value if the operation succeeded, or null if not
 	/// </summary>
 	T? Value { get; }
-
-	/// <summary>
-	/// HTTP status code - by default returns 200 on success or 500 on failure
-	/// </summary>
-	int StatusCode { get; }
-
-	/// <summary>
-	/// Optional URL to redirect to on success
-	/// </summary>
-	string? RedirectTo { get; }
 }
