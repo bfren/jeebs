262eef47a291e132cc90b829549303fe5c0a99c2
bfren <ben@bcgdesign.com>
2022-04-24 08:50:35 +0100

    Adding LogBool and LogBoolAsync - #324

---
+++ src/Jeebs/MaybeExtensions.LogBoolAsync.cs
@@ -0,0 +1,18 @@
+// Jeebs Rapid Application Development
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
+
+using System.Threading.Tasks;
+using Jeebs.Logging;
+
+namespace Jeebs;
+
+public static partial class MaybeExtensions
+{
+	/// <inheritdoc cref="LogBool(Maybe{bool}, ILog, string, string)"/>
+	public static Task<Maybe<bool>> LogBoolAsync(this Task<Maybe<bool>> @this, ILog log) =>
+		LogBoolAsync(@this, log, "Done.", "Failed.");
+
+	/// <inheritdoc cref="LogBool(Maybe{bool}, ILog, string, string)"/>
+	public static async Task<Maybe<bool>> LogBoolAsync(this Task<Maybe<bool>> @this, ILog log, string done, string failed) =>
+		(await @this).LogBool(log, done, failed);
+}
