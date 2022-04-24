90219d58dd8e8e422cea853803907a6df8563923
bfren <ben@bcgdesign.com>
2022-04-24 08:53:30 +0100

    Adding LogBool tests file - #324

---
+++ tests/Tests.Jeebs/_/MaybeExtensions/LogBool_Tests.cs
@@ -0,0 +1,24 @@
+// Jeebs Unit Tests
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
+
+namespace Jeebs.MaybeExtensions_Tests;
+
+public class LogBool_Tests
+{
+	[Fact]
+	public void Is_Some__Value_True__Calls_Log_Inf__With_Correct_Values()
+	{
+
+	}
+
+	[Fact]
+	public void Is_Some__Value_False__Calls_Log_Err__With_Correct_Values()
+	{
+
+	}
+
+	public void Is_None__Calls_Log_Msg__With_Correct_Values()
+	{
+
+	}
+}
