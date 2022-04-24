48d1ab89d36e723db0d9d9789a12abf51cd78bc5
bfren <ben@bcgdesign.com>
2022-04-24 15:05:03 +0100

    Using latest Sodium.Core - #331

--- tests/Tests.Jeebs.Cryptography/_/StringExtensions/VerifyPassword_Tests.cs
+++ tests/Tests.Jeebs.Cryptography/_/StringExtensions/VerifyPassword_Tests.cs
@@ -1,4 +1,4 @@
-﻿// Jeebs Unit Tests
+// Jeebs Unit Tests
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
 
 namespace Jeebs.Cryptography.StringExtensions_Tests;
@@ -6,7 +6,7 @@ namespace Jeebs.Cryptography.StringExtensions_Tests;
 public sealed class VerifyPassword_Tests
 {
 	private readonly string password = "Password to hash.";
-	private readonly string passwordHash = "$argon2id$v=19$m=131072,t=6,p=1$hlK04aWKBnSzi2Sx2T3f7Q$YP80Zmw9Ef0oixaawEGtrJtDszIs2VeoCbH0Qmc+fUk\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+	private readonly string passwordHash = "$argon2id$v=19$m=131072,t=6,p=1$hlK04aWKBnSzi2Sx2T3f7Q$YP80Zmw9Ef0oixaawEGtrJtDszIs2VeoCbH0Qmc+fUk";
 
 	[Theory]
 	[InlineData(null)]
