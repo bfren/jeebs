e11edd0cb3869c5ceb365d5601941af248f1c1d0
bfren <ben@bcgdesign.com>
2022-04-28 13:39:15 +0100

    Updating Razor app to work with Razor class library - #335

--- apps/AppRazor/App.cs
+++ apps/AppRazor/App.cs
@@ -1,6 +1,18 @@
 // Jeebs Test Applications
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
 
+using Jeebs.Auth.Data.Clients.MySql;
+using Jeebs.Mvc.Auth;
+
 namespace AppRazor;
 
-public sealed class App : Jeebs.Apps.Web.RazorApp { }
+public sealed class App : Jeebs.Apps.Web.RazorApp
+{
+	public override void ConfigureServices(HostBuilderContext ctx, IServiceCollection services)
+	{
+		base.ConfigureServices(ctx, services);
+
+		services.AddAuthentication(ctx.Configuration)
+			.WithData<MySqlDbClient>(true);
+	}
+}
--- apps/AppRazor/Pages/Auth/SignIn.cshtml.cs
+++ apps/AppRazor/Pages/Auth/SignIn.cshtml.cs
@@ -1,11 +1,12 @@
 // Jeebs Test Applications
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
 
+using Jeebs.Auth.Data;
 using Jeebs.Logging;
 
 namespace AppRazor.Pages.Auth;
 
 public sealed class SignInModel : Jeebs.Mvc.Razor.Pages.Auth.SignInModel
 {
-	public SignInModel(ILog<SignInModel> log) : base(log) { }
+	public SignInModel(IAuthDataProvider auth, ILog<SignInModel> log) : base(auth, log) { }
 }
