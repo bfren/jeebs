c478ef94c461e34eafdacc49efecedaad55aa071
bfren <ben@bcgdesign.com>
2022-05-02 11:39:01 +0100

    Updating ToOperator tests to handle '2' case - #345

--- tests/Tests.Jeebs.Data.Clients.PostgreSql/_/SortOrderExtensions/ToOperator_Tests.cs
+++ tests/Tests.Jeebs.Data.Clients.PostgreSql/_/SortOrderExtensions/ToOperator_Tests.cs
@@ -1,4 +1,4 @@
-﻿// Jeebs Unit Tests
+// Jeebs Unit Tests
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
 
 using Jeebs.Data.Enums;
@@ -37,7 +37,7 @@ public class ToOperator_Tests
 	public void Other_Returns_ASC()
 	{
 		// Arrange
-		var value = (SortOrder)Rnd.NumberF.GetInt32(min: 2, max: 1000);
+		var value = (SortOrder)Rnd.NumberF.GetInt32(min: 3, max: 1000);
 
 		// Act
 		var result = value.ToOperator();
