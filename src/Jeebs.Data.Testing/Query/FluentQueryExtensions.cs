68969d65e931a7ed12c8220770dcd54468d8dfd7
bfren <ben@bcgdesign.com>
2022-05-02 09:13:49 +0100

    Using new AssertCollection - #341

--- src/Jeebs.Data.Testing/Query/FluentQueryExtensions.cs
+++ src/Jeebs.Data.Testing/Query/FluentQueryExtensions.cs
@@ -4,6 +4,7 @@
 using Jeebs.Data.Query;
 using NSubstitute.Core;
 using StrongId;
+using Xunit.Sdk;
 
 namespace Jeebs.Data.Testing.Query;
 
@@ -18,12 +19,10 @@ public static class FluentQueryExtensions
 	/// <typeparam name="TEntity">Entity Type</typeparam>
 	/// <typeparam name="TId">Entity ID Type</typeparam>
 	/// <param name="this">Fluent Query object</param>
-	/// <param name="calls">Calls to <paramref name="this"/></param>
-	public static void AssertCalls<TEntity, TId>(this IFluentQuery<TEntity, TId> @this, params Action<ICall>[] calls)
+	/// <param name="inspectors">Call inspectors</param>
+	/// <exception cref="CollectionException"></exception>
+	public static void AssertCalls<TEntity, TId>(this IFluentQuery<TEntity, TId> @this, params Action<ICall>[] inspectors)
 		where TEntity : IWithId<TId>
 		where TId : class, IStrongId, new() =>
-		Assert.Collection(
-			collection: @this.ReceivedCalls(),
-			elementInspectors: calls
-		);
+		FluentQueryHelper.AssertCollection(@this.ReceivedCalls().ToArray(), inspectors);
 }
