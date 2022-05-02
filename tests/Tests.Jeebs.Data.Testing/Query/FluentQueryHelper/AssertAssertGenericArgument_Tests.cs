7defb0f61575417cf1706bb2ad349814541cd108
bfren <ben@bcgdesign.com>
2022-05-02 07:14:42 +0100

    Adding AssertGenericArgument tests - #341

---
+++ tests/Tests.Jeebs.Data.Testing/Query/FluentQueryHelper/AssertAssertGenericArgument_Tests.cs
@@ -0,0 +1,86 @@
+// Jeebs Unit Tests
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
+
+using System.Reflection;
+using NSubstitute.Core;
+using NSubstitute.Extensions;
+using Xunit.Sdk;
+
+namespace Jeebs.Data.Testing.Query.FluentQueryHelper_Tests;
+
+public class AssertAssertGenericArgument_Tests
+{
+	[Fact]
+	public void Calls_GetMethodInfo__Asserts_Collection()
+	{
+		// Arrange
+		var method = Substitute.ForPartsOf<MethodInfo>();
+		method.Configure().GetGenericArguments()
+			.Returns(new[] { typeof(string) });
+		var call = Substitute.For<ICall>();
+		call.GetMethodInfo()
+			.Returns(method);
+
+		// Act
+		var action = () => FluentQueryHelper.AssertGenericArgument<string>(call);
+
+		// Assert
+		action();
+		call.Received().GetMethodInfo();
+	}
+
+	[Fact]
+	public void Calls_GetMethodInfo__Throws_When_Incorrect_Type()
+	{
+		// Arrange
+		var method = Substitute.ForPartsOf<MethodInfo>();
+		method.Configure().GetGenericArguments()
+			.Returns(new[] { typeof(string) });
+		var call = Substitute.For<ICall>();
+		call.GetMethodInfo()
+			.Returns(method);
+
+		// Act
+		var action = () => FluentQueryHelper.AssertGenericArgument<Guid>(call);
+
+		// Assert
+		var ex = Assert.Throws<CollectionException>(action);
+		Assert.Contains("Assert.Equal() Failure", ex.Message);
+	}
+
+	[Fact]
+	public void Calls_GetMethodInfo__Throws_When_No_Generic_Arguments()
+	{
+		// Arrange
+		var method = Substitute.ForPartsOf<MethodInfo>();
+		method.Configure().GetGenericArguments()
+			.Returns(Array.Empty<Type>());
+		var call = Substitute.For<ICall>();
+		call.GetMethodInfo()
+			.Returns(method);
+
+		// Act
+		var action = () => FluentQueryHelper.AssertGenericArgument<string>(call);
+
+		// Assert
+		Assert.Throws<CollectionException>(action);
+	}
+
+	[Fact]
+	public void Calls_GetMethodInfo__Throws_When_Too_Many_Generic_Arguments()
+	{
+		// Arrange
+		var method = Substitute.ForPartsOf<MethodInfo>();
+		method.Configure().GetGenericArguments()
+			.Returns(Enumerable.Repeat(typeof(string), Rnd.NumberF.GetInt32(2, 10)).ToArray());
+		var call = Substitute.For<ICall>();
+		call.GetMethodInfo()
+			.Returns(method);
+
+		// Act
+		var action = () => FluentQueryHelper.AssertGenericArgument<string>(call);
+
+		// Assert
+		Assert.Throws<CollectionException>(action);
+	}
+}
