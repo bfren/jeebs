6a9532b223544c8174a77e2f119ee15305c678e5
bfren <ben@bcgdesign.com>
2022-05-02 10:57:34 +0100

    Renaming parameter - #345

--- src/Jeebs/MaybeExtensions.Log.cs
+++ src/Jeebs/MaybeExtensions.Log.cs
@@ -8,19 +8,19 @@ namespace Jeebs;
 public static partial class MaybeExtensions
 {
 	/// <inheritdoc cref="Log{T}(Maybe{T}, ILog, string)"/>
-	public static Maybe<T> Log<T>(this Maybe<T> @this, ILog log) =>
-		Log(@this, log, "Done: {Value}.");
+	public static Maybe<T> Log<T>(this Maybe<T> @this, ILog usingLog) =>
+		Log(@this, usingLog, "Done: {Value}.");
 
 	/// <summary>
 	/// Log a result - or the reason if <see cref="None{T}"/>
 	/// </summary>
 	/// <typeparam name="T">Maybe type</typeparam>
 	/// <param name="this"></param>
-	/// <param name="log"></param>
+	/// <param name="usingLog"></param>
 	/// <param name="message">Log message - must contain one placeholder for the value.</param>
-	public static Maybe<T> Log<T>(this Maybe<T> @this, ILog log, string message) =>
+	public static Maybe<T> Log<T>(this Maybe<T> @this, ILog usingLog, string message) =>
 		@this.Audit(
-			some: x => log.Inf(message, x ?? new object()),
-			none: log.Msg
+			some: x => usingLog.Inf(message, x ?? new object()),
+			none: usingLog.Msg
 		);
 }
