32414fb98753c5bff3f04956f74e0b2df7471790
bfren <ben@bcgdesign.com>
2022-05-30 13:46:03 +0100

    Adding minimum level override for AspNetCore - #369

--- src/Jeebs.Logging.Serilog/LoggerConfigurationExtensions.cs
+++ src/Jeebs.Logging.Serilog/LoggerConfigurationExtensions.cs
@@ -34,6 +34,9 @@ public static class LoggerConfigurationExtensions
 		// Set the minimum log level
 		_ = @this.MinimumLevel.Is(GetMinimum(null, jeebs.Logging.Minimum));
 
+		// Override minimum level for Microsoft ASP.NET messages
+		_ = @this.MinimumLevel.Override("Microsoft.AspNetCore", GetMinimum(jeebs.Logging.MinimumAspNetCore, jeebs.Logging.Minimum));
+
 		// Configure providers
 		ConfigureProviders(ref @this, jeebs);
 
