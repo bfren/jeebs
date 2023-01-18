1474e7179bd50c72a12716647e07a2f13e2bd6a3
bfren <bf@bfren.dev>
2023-01-18 12:14:12 +0000

    Adding Sqlite client tests - #404

---
+++ tests/Tests.Jeebs.Data.Clients.Sqlite/_/SqliteDbClient/GetQuery_Tests.cs
@@ -0,0 +1,415 @@
+// Jeebs Unit Tests
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
+
+using Jeebs.Collections;
+using Jeebs.Data.Enums;
+using Jeebs.Data.Map;
+using Jeebs.Data.Query;
+
+namespace Jeebs.Data.Clients.Sqlite.SqliteDbClient_Tests;
+
+public class GetQuery_Tests
+{
+	[Fact]
+	public void With_Predicates_Returns_Valid_Select_Query()
+	{
+		// Arrange
+		var schema = Rnd.Str;
+		var name = Rnd.Str;
+		var table = new DbName(schema, name);
+
+		var c0Name = Rnd.Str;
+		var c0Alias = Rnd.Str;
+		var c0 = new Column(table, c0Name, Helpers.CreateInfoFromAlias(c0Alias));
+
+		var c1Name = Rnd.Str;
+		var c1Alias = Rnd.Str;
+		var c1 = new Column(table, c1Name, Helpers.CreateInfoFromAlias(c1Alias));
+
+		var list = new ColumnList(new[] { c0, c1 });
+
+		var p0Column = new Column(table, Rnd.Str, Helpers.CreateInfoFromAlias());
+		var p0Operator = Compare.Like;
+		var p0Value = Rnd.Str;
+
+		var p1Column = new Column(table, Rnd.Str, Helpers.CreateInfoFromAlias());
+		var p1Operator = Compare.MoreThanOrEqual;
+		var p1Value = Rnd.Int;
+
+		var predicates = ImmutableList.Create(new (IColumn, Compare, object)[]
+		{
+			( p0Column, p0Operator, p0Value ),
+			( p1Column, p1Operator, p1Value )
+		});
+
+		var client = new SqliteDbClient();
+
+		var expected = "SELECT" +
+			$" \"{c0Name}\" AS \"{c0Alias}\"," +
+			$" \"{c1Name}\" AS \"{c1Alias}\"" +
+			$" FROM \"{schema}.{name}\"" +
+			$" WHERE \"{p0Column.ColName}\" LIKE @P0" +
+			$" AND \"{p1Column.ColName}\" >= @P1;";
+
+		// Act
+		var (query, param) = client.GetQueryTest(table, list, predicates);
+
+		// Assert
+		Assert.Equal(expected, query);
+		Assert.Collection(param,
+			x =>
+			{
+				Assert.Equal("P0", x.Key);
+				Assert.Equal(p0Value, x.Value);
+			},
+			x =>
+			{
+				Assert.Equal("P1", x.Key);
+				Assert.Equal(p1Value, x.Value);
+			}
+		);
+	}
+
+	[Fact]
+	public void With_Parts_Escapes_From_Table()
+	{
+		// Arrange
+		var (client, v) = SqliteDbClient_Setup.Get();
+		var parts = new QueryParts(v.Table);
+
+		// Act
+		var (query, _) = client.GetQuery(parts);
+
+		// Assert
+		Assert.Contains($"\"{v.Schema}.{v.Name}\"", query);
+	}
+
+	[Fact]
+	public void With_Parts_SelectCount_True_Selects_Count()
+	{
+		// Arrange
+		var (client, v) = SqliteDbClient_Setup.Get();
+		var parts = new QueryParts(v.Table) with { SelectCount = true };
+		var expected = $"SELECT COUNT() FROM \"{v.Schema}.{v.Name}\";";
+
+		// Act
+		var (query, _) = client.GetQuery(parts);
+
+		// Assert
+		Assert.Equal(expected, query);
+	}
+
+	[Fact]
+	public void With_Parts_Select_Empty_Selects_All()
+	{
+		// Arrange
+		var (client, v) = SqliteDbClient_Setup.Get();
+		var parts = new QueryParts(v.Table);
+		var expected = $"SELECT * FROM \"{v.Schema}.{v.Name}\";";
+
+		// Act
+		var (query, _) = client.GetQuery(parts);
+
+		// Assert
+		Assert.Equal(expected, query);
+	}
+
+	[Fact]
+	public void With_Parts_Select_List_Escapes_With_Alias_And_Joins_Columns()
+	{
+		// Arrange
+		var (client, v) = SqliteDbClient_Setup.Get();
+
+		var c0Name = Rnd.Str;
+		var c0Alias = Rnd.Str;
+		var c0 = new Column(v.Table, c0Name, Helpers.CreateInfoFromAlias(c0Alias));
+		var c1Name = Rnd.Str;
+		var c1Alias = Rnd.Str;
+		var c1 = new Column(v.Table, c1Name, Helpers.CreateInfoFromAlias(c1Alias));
+		var parts = new QueryParts(v.Table)
+		{
+			SelectColumns = new ColumnList(new[] { c0, c1 })
+		};
+		var expected = "SELECT" +
+			$" \"{v.Schema}.{v.Name}\".\"{c0Name}\" AS \"{c0Alias}\"," +
+			$" \"{v.Schema}.{v.Name}\".\"{c1Name}\" AS \"{c1Alias}\"" +
+			$" FROM \"{v.Schema}.{v.Name}\";";
+
+		// Act
+		var (query, _) = client.GetQuery(parts);
+
+		// Assert
+		Assert.Equal(expected, query);
+	}
+
+	private static void Test_Joins(Func<QueryParts, ImmutableList<(IColumn, IColumn)>, QueryParts> setJoin, string joinType)
+	{
+		// Arrange
+		var (client, v) = SqliteDbClient_Setup.Get();
+
+		var fromName = Rnd.Str;
+		var from = new Column(v.Table, fromName, Helpers.CreateInfoFromAlias());
+
+		var to0Table = new DbName(Rnd.Str, Rnd.Str);
+		var to0Name = Rnd.Str;
+		var to0 = new Column(to0Table, to0Name, Helpers.CreateInfoFromAlias());
+
+		var to1Table = new DbName(Rnd.Str, Rnd.Str);
+		var to1Name = Rnd.Str;
+		var to1 = new Column(to1Table, to1Name, Helpers.CreateInfoFromAlias());
+
+		var join = ImmutableList.Create(new (IColumn, IColumn)[] { (from, to0), (to0, to1) });
+
+		var parts = setJoin(new(v.Table), join);
+
+		var expected = "SELECT" +
+			$" * FROM \"{v.Schema}.{v.Name}\"" +
+			$" {joinType} JOIN \"{to0Table.Schema}.{to0Table.Name}\"" +
+			$" ON \"{v.Schema}.{v.Name}\".\"{fromName}\" = \"{to0Table.Schema}.{to0Table.Name}\".\"{to0Name}\"" +
+			$" {joinType} JOIN \"{to1Table.Schema}.{to1Table.Name}\"" +
+			$" ON \"{to0Table.Schema}.{to0Table.Name}\".\"{to0Name}\" = \"{to1Table.Schema}.{to1Table.Name}\".\"{to1Name}\";";
+
+		// Act
+		var (query, _) = client.GetQuery(parts);
+
+		// Assert
+		Assert.Equal(expected, query);
+	}
+
+	[Fact]
+	public void With_Parts_Adds_Inner_Joins()
+	{
+		Test_Joins((p, j) => p with { InnerJoin = j }, "INNER");
+	}
+
+	[Fact]
+	public void With_Parts_Adds_Left_Joins()
+	{
+		Test_Joins((p, j) => p with { LeftJoin = j }, "LEFT");
+	}
+
+	[Fact]
+	public void With_Parts_Adds_Right_Joins()
+	{
+		Test_Joins((p, j) => p with { RightJoin = j }, "RIGHT");
+	}
+
+	[Fact]
+	public void With_Parts_Adds_Where_Columns_With_Table_Names()
+	{
+		// Arrange
+		var (client, v) = SqliteDbClient_Setup.Get();
+
+		var c0Table = new DbName(Rnd.Str, Rnd.Str);
+		var c0Name = Rnd.Str;
+		var c0 = new Column(c0Table, c0Name, Helpers.CreateInfoFromAlias());
+
+		var c1Table = new DbName(Rnd.Str, Rnd.Str);
+		var c1Name = Rnd.Str;
+		var c1 = new Column(c1Table, c1Name, Helpers.CreateInfoFromAlias());
+
+		var where = ImmutableList.Create(new (IColumn, Compare, object)[]
+		{
+			(c0, Compare.Like, Rnd.Str),
+			(c1, Compare.MoreThanOrEqual, Rnd.Int)
+		});
+
+		var parts = new QueryParts(v.Table) { Where = where };
+
+		var expected = "SELECT *" +
+			$" FROM \"{v.Schema}.{v.Name}\"" +
+			$" WHERE \"{c0Table.Schema}.{c0Table.Name}\".\"{c0Name}\" LIKE @P0" +
+			$" AND \"{c1Table.Schema}.{c1Table.Name}\".\"{c1Name}\" >= @P1;";
+
+		// Act
+		var (query, _) = client.GetQuery(parts);
+
+		// Assert
+		Assert.Equal(expected, query);
+	}
+
+	[Fact]
+	public void With_Parts_Adds_Custom_Where_Clause_And_Parameters()
+	{
+		// Arrange
+		var (client, v) = SqliteDbClient_Setup.Get();
+
+		var w0 = Rnd.Str;
+		var w1 = Rnd.Str;
+		var p0 = Rnd.Str;
+		var p1 = Rnd.Str;
+		var p2 = Rnd.Str;
+		var parametersToAdd0 = new QueryParametersDictionary
+		{
+			{ nameof(p0), p0 },
+			{ nameof(p1), p1 }
+		};
+		var parametersToAdd1 = new QueryParametersDictionary
+		{
+			{ nameof(p2), p2 }
+		};
+
+		var parts = new QueryParts(v.Table)
+		{
+			WhereCustom = ImmutableList.Create(new (string, IQueryParametersDictionary)[]
+			{
+				(w0, parametersToAdd0),
+				(w1, parametersToAdd1)
+			})
+		};
+
+		var expected = $"SELECT * FROM \"{v.Schema}.{v.Name}\" WHERE ({w0}) AND ({w1});";
+
+		// Act
+		var (query, param) = client.GetQuery(parts);
+
+		// Assert
+		Assert.Equal(expected, query);
+		Assert.Collection(param,
+			x =>
+			{
+				Assert.Equal(nameof(p0), x.Key);
+				Assert.Equal(p0, x.Value);
+			},
+			x =>
+			{
+				Assert.Equal(nameof(p1), x.Key);
+				Assert.Equal(p1, x.Value);
+			},
+			x =>
+			{
+				Assert.Equal(nameof(p2), x.Key);
+				Assert.Equal(p2, x.Value);
+			}
+		);
+	}
+
+	[Fact]
+	public void With_Parts_Sets_Parameters()
+	{
+		// Arrange
+		var (client, v) = SqliteDbClient_Setup.Get();
+
+		var c0Table = new DbName(Rnd.Str);
+		var c0Name = Rnd.Str;
+		var c0Value = Rnd.Str;
+		var c0 = new Column(c0Table, c0Name, Helpers.CreateInfoFromAlias());
+
+		var c1Table = new DbName(Rnd.Str);
+		var c1Name = Rnd.Str;
+		var c1Value = Rnd.Int;
+		var c1 = new Column(c1Table, c1Name, Helpers.CreateInfoFromAlias());
+
+		var where = ImmutableList.Create(new (IColumn, Compare, object)[]
+		{
+			(c0, Compare.Like, c0Value),
+			(c1, Compare.MoreThanOrEqual, c1Value)
+		});
+
+		var parts = new QueryParts(v.Table) { Where = where };
+
+		// Act
+		var (_, param) = client.GetQuery(parts);
+
+		// Assert
+		Assert.Collection(param,
+			x =>
+			{
+				Assert.Equal("P0", x.Key);
+				Assert.Equal(c0Value, x.Value);
+			},
+			x =>
+			{
+				Assert.Equal("P1", x.Key);
+				Assert.Equal(c1Value, x.Value);
+			}
+		);
+	}
+
+	[Fact]
+	public void With_Parts_Adds_Order_By_Random()
+	{
+		// Arrange
+		var (client, v) = SqliteDbClient_Setup.Get();
+		var parts = new QueryParts(v.Table)
+		{
+			SortRandom = true
+		};
+
+		var expected = $"SELECT * FROM \"{v.Schema}.{v.Name}\" ORDER BY random();";
+
+		// Act
+		var (query, _) = client.GetQuery(parts);
+
+		// Assert
+		Assert.Equal(expected, query);
+	}
+
+	[Fact]
+	public void With_Parts_Adds_Order_By_With_Table_Name()
+	{
+		// Arrange
+		var (client, v) = SqliteDbClient_Setup.Get();
+
+		var sort0Table = new DbName(Rnd.Str, Rnd.Str);
+		var sort0Name = Rnd.Str;
+		var sort0 = new Column(sort0Table, sort0Name, Helpers.CreateInfoFromAlias());
+
+		var sort1Table = new DbName(Rnd.Str, Rnd.Str);
+		var sort1Name = Rnd.Str;
+		var sort1 = new Column(sort1Table, sort1Name, Helpers.CreateInfoFromAlias());
+
+		var parts = new QueryParts(v.Table)
+		{
+			Sort = ImmutableList.Create(new (IColumn, SortOrder)[]
+			{
+				(sort0, SortOrder.Ascending),
+				(sort1, SortOrder.Descending)
+			})
+		};
+
+		var expected = "SELECT" +
+			$" * FROM \"{v.Schema}.{v.Name}\" ORDER BY" +
+			$" \"{sort0Table.Schema}.{sort0Table.Name}\".\"{sort0Name}\" ASC," +
+			$" \"{sort1Table.Schema}.{sort1Table.Name}\".\"{sort1Name}\" DESC;";
+
+		// Act
+		var (query, _) = client.GetQuery(parts);
+
+		// Assert
+		Assert.Equal(expected, query);
+	}
+
+	[Fact]
+	public void With_Parts_Adds_Limit()
+	{
+		// Arrange
+		var (client, v) = SqliteDbClient_Setup.Get();
+		var max = Rnd.ULng;
+		var parts = new QueryParts(v.Table) { Maximum = max };
+		var expected = $"SELECT * FROM \"{v.Schema}.{v.Name}\" LIMIT {max};";
+
+		// Act
+		var (query, _) = client.GetQuery(parts);
+
+		// Assert
+		Assert.Equal(expected, query);
+	}
+
+	[Fact]
+	public void With_Parts_Adds_Limit_And_Offset()
+	{
+		// Arrange
+		var (client, v) = SqliteDbClient_Setup.Get();
+		var skip = Rnd.ULng;
+		var max = Rnd.ULng;
+		var parts = new QueryParts(v.Table) { Skip = skip, Maximum = max };
+		var expected = $"SELECT * FROM \"{v.Schema}.{v.Name}\" LIMIT {skip}, {max};";
+
+		// Act
+		var (query, _) = client.GetQuery(parts);
+
+		// Assert
+		Assert.Equal(expected, query);
+	}
+}
---
+++ tests/Tests.Jeebs.Data.Clients.Sqlite/_/SqliteDbClient/GetRetrieveQuery_Tests.cs
@@ -0,0 +1,50 @@
+// Jeebs Unit Tests
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
+
+using System.Reflection;
+using Jeebs.Data.Map;
+
+namespace Jeebs.Data.Clients.Sqlite.SqliteDbClient_Tests;
+
+public class GetRetrieveQuery_Tests
+{
+	[Fact]
+	public void Returns_Valid_Select_Query()
+	{
+		// Arrange
+		var schema = Rnd.Str;
+		var name = Rnd.Str;
+		var table = new DbName(schema, name);
+
+		var c0Name = Rnd.Str;
+		var c0Alias = Rnd.Str;
+		var c0 = new Column(table, c0Name, Helpers.CreateInfoFromAlias(c0Alias));
+
+		var c1Name = Rnd.Str;
+		var c1Alias = Rnd.Str;
+		var c1 = new Column(table, c1Name, Helpers.CreateInfoFromAlias(c1Alias));
+
+		var c2Name = Rnd.Str;
+		var c2Alias = Rnd.Str;
+		var c2Property = Substitute.ForPartsOf<PropertyInfo>();
+		c2Property.Name.Returns(c2Alias);
+		var c2 = new Column(table, c2Name, c2Property);
+
+		var list = new ColumnList(new[] { c0, c1 });
+		var client = new SqliteDbClient();
+
+		var id = Rnd.Lng;
+
+		var expected = "SELECT" +
+			$" \"{c0Name}\" AS \"{c0Alias}\"," +
+			$" \"{c1Name}\" AS \"{c1Alias}\"" +
+			$" FROM \"{schema}.{name}\"" +
+			$" WHERE \"{c2Name}\" = {id};";
+
+		// Act
+		var result = client.GetRetrieveQueryTest(table, list, c2, id);
+
+		// Assert
+		Assert.Equal(expected, result);
+	}
+}
