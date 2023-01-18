714ba44356d300a24b2718171d49b7a841dcfe6c
bfren <bf@bfren.dev>
2023-01-18 13:24:25 +0000

    Don't use stored procedure to update user sign in - #404

--- src/Jeebs.Auth.Data.Clients.PostgreSql/PostgreSqlDbClient.cs
+++ src/Jeebs.Auth.Data.Clients.PostgreSql/PostgreSqlDbClient.cs
@@ -2,6 +2,7 @@
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
 
 using System;
+using Jeebs.Auth.Data.Tables;
 using Npgsql;
 using SimpleMigrations;
 using SimpleMigrations.Console;
@@ -12,6 +13,15 @@ namespace Jeebs.Auth.Data.Clients.PostgreSql;
 /// <inheritdoc cref="IAuthDbClient"/>
 public sealed class PostgreSqlDbClient : Jeebs.Data.Clients.PostgreSql.PostgreSqlDbClient, IAuthDbClient
 {
+	/// <inheritdoc/>
+	public string GetUpdateUserLastSignInQuery() =>
+		$@"
+			UPDATE {AuthDb.Schema}.{AuthUserTable.TableName}
+			SET {new AuthUserTable().LastSignedIn} = NOW()
+			WHERE {new AuthUserTable().Id} = @id
+			RETURNING *;
+		";
+
 	private static void DoMigration(string connectionString, Action<SimpleMigrator> act)
 	{
 		// Connection to database
