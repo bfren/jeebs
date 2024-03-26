93fdd341607355ddb098e2dc47707220ba8eefd8
bfren <bf@bfren.dev>
2024-03-26 09:41:32 +0000

    Removing unnecessary Message classes

--- src/Jeebs/Messages/INotFoundMsg.cs
+++
@@ -1,9 +0,0 @@
-// Jeebs Rapid Application Development
-// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
-
-namespace Jeebs.Messages;
-
-/// <summary>
-/// Framework 'Not Found' message
-/// </summary>
-public interface INotFoundMsg { }
