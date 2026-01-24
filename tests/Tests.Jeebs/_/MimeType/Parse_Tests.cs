c85942911beac38cc6686598add7f788dc627bd9
bfren <bf@bfren.dev>
2026-01-24 16:54:16 +0000

    Fixing xunit warning about MemberData

--- tests/Tests.Jeebs/_/MimeType/Parse_Tests.cs
+++ tests/Tests.Jeebs/_/MimeType/Parse_Tests.cs
@@ -6,33 +6,30 @@ namespace Jeebs.MimeType_Tests;
 public class Parse_Tests
 {
 	public static TheoryData<string, MimeType> Returns_Correct_MimeType_Data =>
-		new()
-		{
-			{ string.Empty, MimeType.Blank },
-			{ "application/octet-stream", MimeType.General },
-			{ "image/bmp", MimeType.Bmp },
-			{ "application/msword", MimeType.Doc },
-			{ "application/vnd.openxmlformats-officedocument.wordprocessingml.document", MimeType.Docx },
-			{ "image/gif", MimeType.Gif },
-			{ "image/jpeg", MimeType.Jpg },
-			{ "audio/mp4", MimeType.M4a },
-			{ "audio/mp3", MimeType.Mp3 },
-			{ "application/pdf", MimeType.Pdf },
-			{ "image/png", MimeType.Png },
-			{ "application/vnd.ms-powerpoint", MimeType.Ppt },
-			{ "application/vnd.openxmlformats-officedocument.presentationml.presentation", MimeType.Pptx },
-			{ "application/x-rar-compressed", MimeType.Rar },
-			{ "application/x-tar", MimeType.Tar },
-			{ "text/plain", MimeType.Text },
-			{ "application/vnd.ms-excel", MimeType.Xls },
-			{ "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", MimeType.Xlsx },
-			{ "application/zip", MimeType.Zip }
-		};
+		[
+			(string.Empty, MimeType.Blank),
+			("application/octet-stream", MimeType.General),
+			("image/bmp", MimeType.Bmp),
+			("application/msword", MimeType.Doc),
+			("application/vnd.openxmlformats-officedocument.wordprocessingml.document", MimeType.Docx),
+			("image/gif", MimeType.Gif),
+			("image/jpeg", MimeType.Jpg),
+			("audio/mp4", MimeType.M4a),
+			("audio/mp3", MimeType.Mp3),
+			("application/pdf", MimeType.Pdf),
+			("image/png", MimeType.Png),
+			("application/vnd.ms-powerpoint", MimeType.Ppt),
+			("application/vnd.openxmlformats-officedocument.presentationml.presentation", MimeType.Pptx),
+			("application/x-rar-compressed", MimeType.Rar),
+			("application/x-tar", MimeType.Tar),
+			("text/plain", MimeType.Text),
+			("application/vnd.ms-excel", MimeType.Xls),
+			("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", MimeType.Xlsx),
+			("application/zip", MimeType.Zip)
+		];
 
 	[Theory]
-#pragma warning disable xUnit1044 // Avoid using TheoryData type arguments that are not serializable
 	[MemberData(nameof(Returns_Correct_MimeType_Data))]
-#pragma warning restore xUnit1044 // Avoid using TheoryData type arguments that are not serializable
 	public void Returns_Correct_MimeType(string name, MimeType type)
 	{
 		// Arrange
