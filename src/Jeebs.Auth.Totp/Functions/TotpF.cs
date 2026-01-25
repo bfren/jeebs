ae05907e986aba8baae3c7a1d4a20efd2fd80cf9
bfren <bf@bfren.dev>
2026-01-25 18:30:17 +0000

    Removing auth source files

--- src/Jeebs.Auth.Totp/Functions/TotpF.cs
+++
@@ -1,9 +0,0 @@
-// Jeebs Rapid Application Development
-// Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
-
-namespace Jeebs.Auth.Totp.Functions;
-
-/// <summary>
-/// TOTP Functions.
-/// </summary>
-public static partial class TotpF { }
