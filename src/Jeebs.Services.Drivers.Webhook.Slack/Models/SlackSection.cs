5249df5e121a3bfca488c9fbc01ee0e47c1cb741
bfren <bf@bfren.dev>
2026-02-01 10:18:07 +0000

    Code cleanup - #536

--- src/Jeebs.Services.Drivers.Webhook.Slack/Models/SlackSection.cs
+++ src/Jeebs.Services.Drivers.Webhook.Slack/Models/SlackSection.cs
@@ -2,7 +2,6 @@
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2013
 
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json.Serialization;
 
 namespace Jeebs.Services.Drivers.Webhook.Slack.Models;
